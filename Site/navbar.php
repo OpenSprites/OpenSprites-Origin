71a60d354b9502f12962ebcce42c025302b8ce94
109C <YeMerryNonsense@gmail.com>
2015-02-07 18:49:17 -0500

    Fixed image links

--- Site/navbar.php
+++ Site/navbar.php
@@ -8,11 +8,11 @@
 	<div id="main">
 		<ul>
 		    <a href="index.php">
-			<img id="oslogo" src="http://opensprites.x10.mx/os-logo-2.svg" alt="OpenSprites logo"></img>
+			<img id="oslogo" src="assets/images/os-logo-2.svg" alt="OpenSprites logo"></img>
 			<a href="upload.php"><li class="one">Upload</li></a>
-			<a href="http://opensprites.x10.mx/"><li class="two">Resources</li></a>
+			<a href="./"><li class="two">Resources</li></a>
 			<a href="https://github.com/OpenSprites/"><li class="two">Development</li></a>
-			<a href="http://opensprites.x10.mx/"><li class="two">About</li></a>
+			<a href="./"><li class="two">About</li></a>
 			<?php
 			  if(!isset($_SESSION["username"])){
 			?>
@@ -28,7 +28,7 @@
             ?>
               <a>
                 <li class="two">
-			      <a href="http://opensprites.x10.mx/"><li class="two">My account</li></a>
+			      <a href="./"><li class="two">My account</li></a>
 			      <?php echo $_SESSION["username"]; ?>
 			      <!-- Maybe an image? -->
                 </li>
