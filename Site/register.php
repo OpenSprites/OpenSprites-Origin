2b8b18f62ab4ffe78b158d0a10e3e81934764ea4
Firedrake969 <Firedrake969@users.noreply.github.com>
2015-02-09 15:20:50 -0700

    Update register.php

--- Site/register.php
+++ Site/register.php
@@ -10,7 +10,7 @@
   <body>
   <?php include 'navbar.php'; ?>
   <div style="padding:35px;">
-    <form enctype="multipart/form-data" action="auth/handle_register.php" method="POST">
+    <form enctype="multipart/form-data" action="register.php" method="POST">
       Username: <input name="username" type="text" /><br>
       Password: <input name="password" type="password" /><br>
       Confirm password:  <input name="confirm_assword" type="password" /><br>
