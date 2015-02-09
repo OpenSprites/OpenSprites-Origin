6b31a2ff136a689395506105f33b3d3256eff37f
109C <YeMerryNonsense@gmail.com>
2015-02-08 21:46:53 -0500

    Added changing passwords

---
+++ Site/change_password.php
@@ -0,0 +1,28 @@
+<?php
+  session_start();
+  include 'auth/AuthFunctions.php';
+  include 'config.php';
+  
+  if(!isset($_SESSION["username"])){
+    header('Location: ./');
+  }
+  if(!isset($_POST["OldPassword"]) || !isset($_POST["NewPassword"])){
+    header('Location: ./show_error.php?e=Please%20enter%20your%20old%20password%20and%20new%20password');
+    exit;
+  }
+  $username = $_SESSION["username"];
+  $password_hash = GetHashForName($username);
+  if(!isset($password_hash)){
+    header('Location: ./show_error.php?e=No%20hash%20found');
+    exit;
+  }
+
+  if(password_verify($_POST["OldPassword"], $password_hash)){
+    ReplaceNameHash($_SESSION["username"], password_hash($_POST["NewPassword"], PASSWORD_BCRYPT));
+    header('Location: ./');
+    exit;
+  }else{
+    header('Location: ./show_error.php?e=Invalid%20Password');
+    exit;
+  }
+?>
