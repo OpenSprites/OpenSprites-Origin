8b4cd23e7a32d511b20ccf2a5a641065d4c8226e
Firedrake969 <Firedrake969@users.noreply.github.com>
2015-02-13 13:54:27 -0700

    Update index.php
    we're using mysql, and please no inline styling.

--- Site/register/index.php
+++ Site/register/index.php
@@ -4,6 +4,7 @@
   include '../config.php';
   
   //connect to server and login check via cookie
+  include '../assets/includes/login_check.php';  //to be added later!
   
   if (isset($_SESSION['username'])) {
     header("Location: /");
@@ -17,21 +18,13 @@
   </head>
   <body>
   <?php include 'navbar.php'; ?>
-  <div style="padding:35px;">
-    <?php if($USE_MSQLI){ ?>
+  <div>
     <form enctype="multipart/form-data" action="register.php" method="POST">
       Username: <input name="username" type="text" /><br>
       Password: <input name="password" type="password" /><br>
       Confirm password:  <input name="confirm_password" type="password" /><br>
       <input type="Submit"/>
     </form>
-    <?php }else{ ?>
-    <form enctype="multipart/form-data" action="../auth/handle_register.php" method="POST">
-      Username: <input name="FormUsername" type="text" /><br>
-      Password: <input name="FormPassword" type="password" /><br>
-      <input type="Submit"/>
-    </form>
-    <?php } ?>
   </div>
   <?php include 'Footer.html'; ?>
   </body>
