f0299fddf98edeaaf1a3b030f1ae0e204644631e
Firedrake969 <Firedrake969@users.noreply.github.com>
2015-02-14 10:21:50 -0700

    Update avatar.php
    scalable function

--- Site/assets/includes/avatar.php
+++ Site/assets/includes/avatar.php
@@ -1,6 +1,9 @@
 <?php
   // gets the user's image src
-  $raw_json = file_get_contents("http://scratch.mit.edu/site-api/users/all/" . $_GET['username'] . "/");
-  $user_arr = json_decode($raw_json, true);
-  $user_avatar = $user_arr["thumbnail_url"];
-  // no closing tag for a reason
+  function display_user_avatar($username_grabbed, $image_size) {
+    $raw_json = file_get_contents("http://scratch.mit.edu/site-api/users/all/" . $username_grabbed . "/");
+    $user_arr = json_decode($raw_json, true);
+    $user_avatar = $user_arr["thumbnail_url"];
+    echo "<img class='profile-image $image_size' src='http:" . $user_avatar . "'";
+  }
+?>
