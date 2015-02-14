863fb05c4bd7528d5d5e5da8461918048054aea2
Firedrake969 <Firedrake969@users.noreply.github.com>
2015-02-14 10:37:04 -0700

    Update avatar.php

--- Site/assets/includes/avatar.php
+++ Site/assets/includes/avatar.php
@@ -1,9 +1,17 @@
 <?php
   // user image
-  function display_user_avatar($username_grabbed, $image_size) {
+  function grab_user_avatar($username_grabbed) {
     $raw_json = file_get_contents("http://scratch.mit.edu/site-api/users/all/" . $username_grabbed . "/");
     $user_arr = json_decode($raw_json, true);
     $user_avatar = $user_arr["thumbnail_url"];
-    echo "<img class='profile-image' style='width:" . $image_size . "px;height:" . $image_size . "px;' src='http:" . $user_avatar . "'>";
+    return "http:$user_avatar";
+  }
+  
+  function set_user_avatar($username) {
+    
+  }
+  
+  function display_user_avatar($username, $size) {
+    
   }
 ?>
