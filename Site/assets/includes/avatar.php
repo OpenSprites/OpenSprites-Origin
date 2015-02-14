b271c206fd1912ece38edf082c3f359e84f75437
Alex Bates <16batesa@gmail.com>
2015-02-14 16:26:29 +0000

    Update avatar.php

--- Site/assets/includes/avatar.php
+++ Site/assets/includes/avatar.php
@@ -2,5 +2,6 @@
 // gets the user's image and echos it
 $encoded = file_get_contents('http://scratch.mit.edu/site-api/users/all/' . $_GET['u'] . '/');
 $decoded = json_decode($encoded);
+echo 'Test';
 echo $decoded;
 ?>
