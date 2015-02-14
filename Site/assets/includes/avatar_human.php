9f645d2c81c5d8bed36e580db2a67dcdddf144b6
Firedrake969 <Firedrake969@users.noreply.github.com>
2015-02-14 10:15:14 -0700

    Delete avatar_human.php
    unneeded

--- Site/assets/includes/avatar_human.php
+++
@@ -1,7 +0,0 @@
-<?php
-  // gets the user's image src
-  $raw_json = file_get_contents("http://scratch.mit.edu/site-api/users/all/" . $_GET['username'] . "/");
-  $user_arr = json_decode($raw_json, true);
-  $user_avatar = $user_arr["thumbnail_url"];
-  echo '<img style="position:absolute;top:0px;left:0px;width:60px;height:60px;" src="http:' . $user_avatar . '">';
-?>
