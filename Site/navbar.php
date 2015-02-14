0a09acae40f505f51debe28bbf0a0ceaf43b7231
Firedrake969 <Firedrake969@users.noreply.github.com>
2015-02-14 07:58:05 -0700

    Create navbar.php
    in process of cleaning up/fixing - PLEASE DO NOT CHANGE!

---
+++ Site/navbar.php
@@ -0,0 +1,51 @@
+<div class="main">
+    <ul> <a href="index.php">
+			<img id="oslogo" src="assets/images/os-logo-2.svg" alt="OpenSprites logo"></img>
+			<a href="upload.php"><li class="one">Upload</li></a>
+	<a href="./"><li class="two">Resources</li></a>
+	<a href="https://github.com/OpenSprites/"><li class="two">Development</li></a>
+	<a href="./"><li class="two">About</li></a>
+
+        <?php if(!isset($_SESSION[ "username"])){ ?>
+        
+        <a href="login.php"><li class="two">
+              Login
+            </li></a>
+ <a href="register.php"><li class="two">
+              Create a free account
+     <img src="assets/images/Hamburger.png" width="10" height="10" />
+            </li></a>
+
+        <?php }else{ ?> <a>
+        
+                <li class="two">
+			      <a href="account.php"><li class="two">My account</li></a>
+
+        <?php echo $_SESSION[ "username"]; ?>
+        </li>
+        </a>
+        
+        <?php } ?>
+        
+    </ul>
+</div>
+<div class="header">
+    <div class="container">	<a class="scratch" href=""></a>
+
+        <ul class="left">
+            <li><a href="">Sprites</a>
+            </li>
+            <li><a href="">Scripts</a>
+            </li>
+            <li class="last"><a href="">Media</a>
+            </li>
+        </ul>
+        <ul class="right">
+            <li><a href="/live/alpha/register/">Sign Up</a>
+            </li>
+            <li class="last" id='login'>Log In
+                <div id='login-menu'>login form would pop up when you hit login</div>
+            </li>
+        </ul>
+    </div>
+</div>
