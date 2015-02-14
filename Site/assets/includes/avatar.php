31d48c3e7ca6fbf2485e3778c206742dfc0e28ec
Alex Bates <16batesa@gmail.com>
2015-02-14 17:31:20 +0000

    moved avatar.php to global_functions.php

--- Site/assets/includes/avatar.php
+++
@@ -1,9 +0,0 @@
-<?php
-  // gets the user's image src
-  function display_user_avatar($username_grabbed, $image_size) {
-    $raw_json = file_get_contents("http://scratch.mit.edu/site-api/users/all/" . $username_grabbed . "/");
-    $user_arr = json_decode($raw_json, true);
-    $user_avatar = $user_arr["thumbnail_url"];
-    echo "<img class='profile-image' style='width:" . $image_size . "px;height:" . $image_size . "px;' src='http:" . $user_avatar . "'>";
-  }
-?>
