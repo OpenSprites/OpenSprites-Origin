0055293f4d0e12864504fa20890dbeb4a1017938
Alex Bates <16batesa@gmail.com>
2015-02-14 09:25:05 +0000

    Update index.php

--- Site/register/index.php
+++ Site/register/index.php
@@ -14,18 +14,28 @@
 <!DOCTYPE html>
 <html>
   <head>
-  <?php include 'Header.html'; ?>
+    <!--Imports the metadata and information that will go in the <head> of every page-->
+  	<?php echo file_get_contents('../Header.html'); ?>
+  	<!-- do we really need this?  it should be in header.html -->
+  	<LINK REL=StyleSheet HREF="main.css" TYPE="text/css" MEDIA=screen>
   </head>
   <body>
-  <?php include 'navbar.php'; ?>
-  <div>
-    <form enctype="multipart/form-data" action="register.php" method="POST">
-      Username: <input name="username" type="text" /><br>
-      Password: <input name="password" type="password" /><br>
-      Confirm password:  <input name="confirm_password" type="password" /><br>
-      <input type="Submit"/>
-    </form>
-  </div>
-  <?php include 'Footer.html'; ?>
+    <?php include '../navbar.php'; ?>
+    
+    <div class="container main">
+      <div class="main-inner">
+        <h1 id="opensprites-heading">Register</h1>
+        <p>
+          <form enctype="multipart/form-data" action="register.php" method="POST">
+            Username: <input name="username" type="text" /><br>
+            Password: <input name="password" type="password" /><br>
+            Confirm password:  <input name="confirm_password" type="password" /><br>
+            <input type="Submit"/>
+          </form>
+        </p>
+      </div>
+    </div>
+    
+    <?php echo file_get_contents('../footer.html'); ?>
   </body>
 </html>
