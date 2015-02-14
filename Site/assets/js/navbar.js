bfa4fb2cde725a7da9828ce2e73a7ca6f811ca68
Firedrake969 <firedrake969@users.noreply.github.com>
2015-02-14 12:31:36 -0700

    js
    Don't wrap 2 $() in each other.

--- Site/assets/js/navbar.js
+++ Site/assets/js/navbar.js
@@ -1,20 +1,19 @@
-//to be js for navbar dropdown
-(function() {  //wrap everything w/(function() {}) to keep it safe
+(function () { //wrap everything w/(function() {}) to keep it safe
 
-var isShown = false;
+    var isShown = false;
 
-$(function() {  //document.ready
-	$('#login-popup').hide();
-	
-	$(function() {  //document.ready
-		$('#login').click(function() {
-			//stuff!
-			$('#login').click(function() {
-			if (isShown) {
-				$('#login-popup').fadeOut(250);
-			} else {
-				$('#login-popup').fadeIn(250);
-			}
-		});
-	});
+    $(function () { //document.ready
+        $('#login-popup').hide();
+
+        $('#login').click(function () {
+            if (isShown) {
+                $('#login-popup').fadeOut(250);
+				isShown = !isShown;
+            } else {
+                $('#login-popup').fadeIn(250);
+				isShown = !isShown;
+            }
+        });
+    });
+
 })(); 
