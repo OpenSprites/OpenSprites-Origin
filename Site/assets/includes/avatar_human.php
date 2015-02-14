2270198adfb59315925973654f5c35c7b9db9091
Alex Bates <16batesa@gmail.com>
2015-02-14 17:07:55 +0000

    Update avatar_human.php

--- Site/assets/includes/avatar_human.php
+++ Site/assets/includes/avatar_human.php
@@ -1,7 +1,7 @@
 <?php
   // gets the user's image src
   $raw_json = file_get_contents("http://scratch.mit.edu/site-api/users/all/" . $_GET['username'] . "/");
-  $user_arr = json_decode($raw_json);
+  $user_arr = json_decode($raw_json, true);
   $user_avatar = $user_arr["thumbnail_url"];
   echo '<img src="http:' . $user_avatar . '">';
 ?>
