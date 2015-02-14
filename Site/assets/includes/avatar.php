19adbb0dfd39dde37680ec6ef57259f0d01f0da7
Firedrake969 <Firedrake969@users.noreply.github.com>
2015-02-14 09:32:03 -0700

    Update avatar.php

--- Site/assets/includes/avatar.php
+++ Site/assets/includes/avatar.php
@@ -1,7 +1,6 @@
 <?php
 // gets the user's image and echos it
-$encoded = file_get_contents('http://scratch.mit.edu/site-api/users/all/' . $_GET['u'] . '/');
+$encoded = file_get_contents('http://scratch.mit.edu/site-api/users/all/' . $_GET['username'] . '/');
 $decoded = json_decode($encoded);
 echo 'Test';
-echo $decoded;
-?>
+echo $decoded;  //removed closing tag for a reason - this will be included into the register.php page.
