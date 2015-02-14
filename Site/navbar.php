2a8289998759d5d92e3e1cf0ec32aa27227da0bc
Firedrake969 <Firedrake969@users.noreply.github.com>
2015-02-14 09:30:48 -0700

    Update navbar.php

--- Site/navbar.php
+++ Site/navbar.php
@@ -1,39 +1,3 @@
-<!--
-<div class="main">
-    <ul> <a href="index.php">
-			<img id="oslogo" src="assets/images/os-logo-2.svg" alt="OpenSprites logo"></img>
-			<a href="upload.php"><li class="one">Upload</li></a>
-	<a href="./"><li class="two">Resources</li></a>
-	<a href="https://github.com/OpenSprites/"><li class="two">Development</li></a>
-	<a href="./"><li class="two">About</li></a>
-
-        <?php if(!isset($_SESSION[ "username"])){ ?>
-        
-        <a href="login.php"><li class="two">
-              Login
-        </li></a>
-	<a href="register.php"><li class="two">
-              Create a free account
-	<img src="assets/images/Hamburger.png" width="10" height="10" />
-        </li></a>
-
-        <?php }else{ ?> <a>
-        
-        <li class="two">
-		<a href="account.php">
-			<li class="two">My account</li>
-		</a>
-
-        <?php echo $_SESSION[ "username"]; ?>
-        
-        </li>
-        </a>
-        
-        <?php } ?>
-        
-    </ul>
-</div>
--->
 <div class="header">
     <div class="container">	<a class="scratch" href=""></a>
 
@@ -54,6 +18,8 @@
                 <div id='login-menu'>login form would pop up when you hit login</div>
             </li>
         </ul>
+        
+        <?php } else  { ?>  <!-- display login info/username/etc -->
         
         <?php } ?>
         
