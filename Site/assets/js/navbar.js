ed3a2421ab7f133a49fde37a08c34799ccda7e1d
Alex Bates <16batesa@gmail.com>
2015-02-14 18:55:57 +0000

    Update navbar.js

--- Site/assets/js/navbar.js
+++ Site/assets/js/navbar.js
@@ -1,11 +1,9 @@
 //to be js for navbar dropdown
 (function() {  //wrap everything w/(function() {}) to keep it safe
-
-$(function() {  //document.ready
-	$('#login-popup').click(function() {
-		//stuff!
-		alert('all is good');
+	$(function() {  //document.ready
+		$('#login-popup').click(function() {
+			//stuff!
+			alert('todo, assigned to grannycookies');
+		});
 	});
-});
-
 })(); 
