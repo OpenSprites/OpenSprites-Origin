7bcf0590558399ed627fa9b21cb5c484fb10b582
Alex Bates <16batesa@gmail.com>
2015-02-14 16:52:09 +0000

    Update avatar.php

--- Site/assets/includes/avatar.php
+++ Site/assets/includes/avatar.php
@@ -1,7 +1,16 @@
 <?php
-  // gets the user's image and echos it
+  // gets the user's image src and echos it
   $raw_json = file_get_contents("http://scratch.mit.edu/site-api/users/all/" . $_GET['username'] . "/");
-  $user_arr = json_decode($raw_json);
-  $user_avatar = $user_arr["thumbnail-url"];
-  echo 'Test';
-  echo $user_avatar;  //removed closing tag for a reason - this will be included into the register.php page.
+  
+  preg_match('/thumbnail_url/', $encoded, $matches, PREG_OFFSET_CAPTURE, 3);
+  $i = $matches[0];
+  $start = $i[1];
+  
+  $short = substr($encoded, $start + 17);
+  
+  preg_match('/", /', $short, $matches, PREG_OFFSET_CAPTURE, 3);
+  $i = $matches[0];
+  $end = $i[1];
+  
+  echo 'http:' . substr($short, 0, $end);
+?>
