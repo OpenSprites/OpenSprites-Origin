703cc200fc44018e4438c7fef75c168b08aca346
Alex Bates <16batesa@gmail.com>
2015-02-14 17:04:44 +0000

    Create avatar_human.php

---
+++ Site/assets/includes/avatar_human.php
@@ -0,0 +1,7 @@
+<?php
+  // gets the user's image src
+  $raw_json = file_get_contents("http://scratch.mit.edu/site-api/users/all/" . $_GET['username'] . "/");
+  $user_arr = json_decode($raw_json);
+  $user_avatar = $user_arr["thumbnail-url"];
+  print_r($user_avatar);
+?>
