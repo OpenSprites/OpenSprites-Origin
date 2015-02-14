63e57a62648d6cbac63ed637760617c9ead33e63
Alex Bates <16batesa@gmail.com>
2015-02-14 16:55:42 +0000

    Update avatar.php

--- Site/assets/includes/avatar.php
+++ Site/assets/includes/avatar.php
@@ -1,16 +1,5 @@
 <?php
-  // gets the user's image src and echos it
-  $encoded = file_get_contents("http://scratch.mit.edu/site-api/users/all/" . $_GET['username'] . "/");
-  
-  preg_match('/thumbnail_url/', $encoded, $matches, PREG_OFFSET_CAPTURE, 3);
-  $i = $matches[0];
-  $start = $i[1];
-  
-  $short = substr($encoded, $start + 17);
-  
-  preg_match('/", /', $short, $matches, PREG_OFFSET_CAPTURE, 3);
-  $i = $matches[0];
-  $end = $i[1];
-  
-  echo 'http:' . substr($short, 0, $end);
-?>
+  // gets the user's image src
+  $raw_json = file_get_contents("http://scratch.mit.edu/site-api/users/all/" . $_GET['username'] . "/");
+  $user_arr = json_decode($raw_json);
+  $user_avatar = $user_arr["thumbnail-url"];
