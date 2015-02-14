9ac28cfaf562ab1086f7300d12f5ebb168407d36
Alex Bates <16batesa@gmail.com>
2015-02-14 20:40:23 +0000

    Update navbar.php

--- Site/navbar.php
+++ Site/navbar.php
@@ -14,15 +14,15 @@
         <?php if(!isset($_SESSION['username'])) { ?>
             <li><a href="/live/alpha/register/">Sign Up</a>
             </li>
-            <li class="last" id='login'><span>Log In</span>
-                <div id='login-popup'>
-					<form method='POST' action='live/alpha/assets/includes/login.php'>
-						<input type='text' name='username'></input>
-						<input type='password' name='password'></input>
-						<input type='submit' value='Submit'></input>
-					</form>
-				</div>
-            </li>
+            <li class="last" id='login'><span>Log In</span></li>
+            <div id='login-popup'>
+				<form method='POST' action='live/alpha/assets/includes/login.php'>
+					<input type='text' name='username'></input>
+					<input type='password' name='password'></input>
+					<input type='submit' value='Submit'></input>
+				</form>
+			</div>
+            
         <?php } else  { ?>  <!-- display login info/username/etc -->
 			<div id='login-display'>Welcome, <?= $_SESSION['username'] ?></div>
 			<div id='login-menu'>
