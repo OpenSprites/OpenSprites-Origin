932e1dce4fc79ecc148ccb430a2defcccc2506b6
Firedrake969 <firedrake969@users.noreply.github.com>
2015-02-14 13:04:10 -0700

    Revert "Update and rename navbar.js to navbar.txt"
    This reverts commit 91fd412d52095f0483afe10e259d20de970efaf2.

--- Site/assets/js/navbar.js
+++ Site/assets/js/navbar.js
@@ -1,16 +1,19 @@
 (function () { //wrap everything for safety
+
     var isShown = false;
+
     $(function () { //document.ready
         $('#login-popup').hide();
 
-        $('#login').on('click', function() {
-        	if (isShown) {
+        $('#login').click(function () {
+            if (isShown) {
                 $('#login-popup').fadeOut(250);
+				isShown = !isShown;
             } else {
                 $('#login-popup').fadeIn(250);
+				isShown = !isShown;
             }
-            
-            isShown = !isShown;
-        })
+        });
     });
+
 })();
