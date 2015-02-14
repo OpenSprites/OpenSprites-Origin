89a05308b14e333089ecf64f8d79d228d88ff8a2
Firedrake969 <Firedrake969@users.noreply.github.com>
2015-02-14 10:05:59 -0700

    Update avatar.php

--- Site/assets/includes/avatar.php
+++ Site/assets/includes/avatar.php
@@ -1,6 +1,6 @@
 <?php
   // gets the user's image src
   $raw_json = file_get_contents("http://scratch.mit.edu/site-api/users/all/" . $_GET['username'] . "/");
-  $user_arr = json_decode($raw_json);
+  $user_arr = json_decode($raw_json, true);
   $user_avatar = $user_arr["thumbnail_url"];
   // no closing tag for a reason
