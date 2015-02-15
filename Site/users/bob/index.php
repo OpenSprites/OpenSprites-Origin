6474caa80a161afbfc2df7b3ee4b5980f57bbaa8
Alex Bates <16batesa@gmail.com>
2015-02-15 09:47:43 +0000

    Update index.php

--- Site/users/bob/index.php
+++ Site/users/bob/index.php
@@ -6,12 +6,12 @@
 <head>
 	<!--Imports the metadata and information that will go in the <head> of every page-->
 	<?php echo file_get_contents('../../Header.html'); ?>
-</head>
-<body>
+	
 	<!--Imports styling-->
 	<link href='../../main-style.css' rel='stylesheet' type='text/css'>
 	<link href='../user-style.css' rel='stylesheet' type='text/css'>
-	
+</head>
+<body>
 	<!--Imports navigation bar-->
 	<?php include "../../navbar.php"; ?>
 	
