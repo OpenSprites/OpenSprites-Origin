527ae84c03724fde802f239572cc964b62e451c1
Alex Bates <16batesa@gmail.com>
2015-02-15 11:04:55 +0000

    Update index.php

--- Site/users/index.php
+++ Site/users/index.php
@@ -3,7 +3,6 @@
 	require "../assets/includes/global_functions.php";
 	
 	$username = $_GET['username'];
-	update_user_avatar($username); // Temporary
 ?>
 <!DOCTYPE html>
 <html>
@@ -24,7 +23,7 @@
 		<div id='username'>
 			<?php echo $username; ?>
 		</div>
-		<?php display_user_avatar($username, 2); ?>
+		<?php temp_display_user_avatar($username, 2); ?>
 	</div>
 
 	<div class="container main" id="collections">
