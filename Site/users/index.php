57f42a9ee1d6dbfbca1219d7a1227d7eb3a67b39
Firedrake969 <firedrake969@users.noreply.github.com>
2015-02-15 10:40:31 -0700

    check registration before entering user page

--- Site/users/index.php
+++ Site/users/index.php
@@ -3,9 +3,15 @@
 	require "../assets/includes/avatar.php";
 	
 	// capitalise username correctly
-	$username = $_GET['username'];
+	$username = mysqli_real_escape_string($connection, $_GET['username']);  //shouldn't do anything, but to be safe...
 	$raw_json = file_get_contents("http://scratch.mit.edu/site-api/users/all/" . $username . "/");
-	if($raw_json == 'FALSE' or $raw_json == FALSE or $raw_json == file_get_contents("http://scratch.mit.edu/404")) {
+	
+	//get username from database - make sure they're registered
+	$check_query = "SELECT username FROM user_data WHERE username=$username";
+	$check_res = mysqli_query($connection, $check_query);
+	$check_rows = mysqli_fetch_assoc($check_res);
+	
+	if($raw_json == 'FALSE' or $raw_json == FALSE or $raw_json == file_get_contents("http://scratch.mit.edu/404") or mysqli_num_rows($check_rows) == 0) {
 		// something went wrong, display 404 error page instead
 		header('Location: /404.html');
 	} else {
