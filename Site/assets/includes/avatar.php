de9f75fbaf570a3ac4ccf6e7a5afeefc9d37b0c7
Alex Bates <16batesa@gmail.com>
2015-02-15 14:31:23 +0000

    Update avatar.php

--- Site/assets/includes/avatar.php
+++ Site/assets/includes/avatar.php
@@ -4,7 +4,7 @@
     $raw_json = file_get_contents("http://scratch.mit.edu/site-api/users/all/" . $username_grabbed . "/");
     $user_arr = json_decode($raw_json, true);
     $user_avatar = $user_arr["thumbnail_url"];
-    return "http:$user_avatar";
+    return "http:" . $user_avatar;
   }
   
   function update_user_avatar($username) {
@@ -23,7 +23,10 @@
   }
   
   function temp_display_user_avatar($username, $size) {
-    $src = grab_user_avatar($username);
+    $raw_json = file_get_contents("http://scratch.mit.edu/site-api/users/all/" . $username_grabbed . "/");
+    $user_arr = json_decode($raw_json, true);
+    $user_avatar = $user_arr["thumbnail_url"];
+    $src = "http:" . $user_avatar;
     echo '<img class="user-avatar ' . $size . '" src="' . $src . '">'
   }
 ?>
