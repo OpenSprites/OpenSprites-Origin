a31f060482566f6c1f0cacf49b70debc45128bdd
Alex Bates <16batesa@gmail.com>
2015-02-15 15:37:29 +0000

    Update index.php

--- Site/users/index.php
+++ Site/users/index.php
@@ -20,12 +20,12 @@
 	<!-- Main wrapper -->
 	<?php echo "<img id='background-img' src='bg/" . $username . "_custom.png' "; ?>onerror='this.src="bg/default.png"'>
 	<div id='dark-overlay'>
-		<div id="user-pane">
+		<div id="user-pane-right">
 			<div id='username'>
 				<?php echo $username; ?>
 			</div>
 		</div>
-		<div id="user-pane">
+		<div id="user-pane-left">
 			<?php
 				display_user_avatar($username, 'x100');
 			?>
