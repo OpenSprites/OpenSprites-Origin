740d2a913895efa71f1bf53268728f74da9d8e94
Alex Bates <16batesa@gmail.com>
2015-02-15 09:43:58 +0000

    Update user.php

--- Site/users/user.php
+++ Site/users/user.php
@@ -1,5 +1,5 @@
 <?php
 
-echo "<div id='dark-overlay'><div id='username'>" . $_GET['username'] . "</div></div>"
+echo "<div id='dark-overlay'><img id='background-img' src='" . $GET_['username'] . "/bg_img.png'></div><div id='username'>" . $_GET['username'] . "</div>"
 
 ?>
