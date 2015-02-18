0934e957760063980cb4c50a07f5c7634c522d65
Andrew <andrewjcolescratch@gmail.com>
2015-02-18 15:41:38 -0400

    Added includes

--- Blog/andrewjcole/index.php
+++ Blog/andrewjcole/index.php
@@ -1,28 +1,4 @@
-<!DOCTYPE html>
-<html>
-<!-- DO NOT EDIT YET! I'm still working on this. -->
-<!-- Still need includes -->
-<head>
-    <title>Blog - OpenSprites Resources</title>
-    <meta http-equiv="Content-Type" content="text/html; charset=ISO-8859-1">
-    <meta name="description" content="OpenSprites Resources. Free and open source resources for your Scratch projects">
-    <meta name="keywords" content="OpenSprites, opensprites, spritesopen, Opensprites, openSprites, OS, programming, resources, free, open source, open, resource, media, sprites, sounds, images, scripts, script, costume, backdrop, background">
-    <meta name="author" content="OpenSprites Team">
-    <meta name="subject" content="Website">
-    <link rel="stylesheet" type="text/css" href="main.css">
-    <link href='http://fonts.googleapis.com/css?family=Open+Sans:300' rel='stylesheet' type='text/css'>
-</head>
-
-<body>
-<noscript>Some features of this page may not work properly without javascript...</noscript>
-    <div id="header">
-        <h1 class="blog" align="center">BLOG</h1>
-        <span id="return"><a id="back" href="#" style="color:white">< Return to OpenSprites</a></span>
-    </div>
-<div id="content" align="left">
-<table style="width:100%">
-  <tr>
-    <td width="80%">    
+<?php include "top.php"; ?>  
         <!-- Article -->
         <div id="article">
 
@@ -39,21 +15,6 @@
         
         </div>
     </td>
-    <!-- RIGHT SIDEBAR -->
-    <td class="sidebar" valign="top" align="center">
-        <h3>OpenSprites Blog</h3>
-            <p class="sidebar-text" align="left">&nbsp;&nbsp;&nbsp;&nbsp;Welcome to the OpenSprites Blog! This page is where you can find updates on OpenSprites as well as cool scripts and sprites highlighted by the admins!</p> 
-        <h3>Team</h3>
-            <img src="http://tinyurl.com/scratchericon">
-            <img src="http://tinyurl.com/scratchericon">
-            <img src="http://tinyurl.com/scratchericon">
-            <img src="http://tinyurl.com/scratchericon">
-    </td> 
-  </tr>
-</table>
-</div>
-<div id="footer">
-    <p align="center">2015 OpenSprites Team</p>
-</div>
-</body>
-</html>
+
+<?php include "sidebar.php"; ?>
+<?php include "bottom.php"; ?>
