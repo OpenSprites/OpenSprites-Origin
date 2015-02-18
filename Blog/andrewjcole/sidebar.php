d2b707199100f4e3962528cf594d137dcc7d4625
Andrew <andrewjcolescratch@gmail.com>
2015-02-18 15:44:09 -0400

    Create sidebar.php

---
+++ Blog/andrewjcole/sidebar.php
@@ -0,0 +1,14 @@
+    </td>
+    <!-- RIGHT SIDEBAR -->
+    <td class="sidebar" valign="top" align="center">
+        <h3>OpenSprites Blog</h3>
+            <p class="sidebar-text" align="left">&nbsp;&nbsp;&nbsp;&nbsp;Welcome to the OpenSprites Blog! This page is where you can find updates on OpenSprites as well as cool scripts and sprites highlighted by the admins!</p> 
+        <h3>Team</h3>
+            <img src="http://tinyurl.com/scratchericon">
+            <img src="http://tinyurl.com/scratchericon">
+            <img src="http://tinyurl.com/scratchericon">
+            <img src="http://tinyurl.com/scratchericon">
+    </td> 
+  </tr>
+</table>
+</div>
