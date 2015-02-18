d3c9657fb7b6fbcdacf0785cd057637e178f384c
Andrew <andrewjcolescratch@gmail.com>
2015-02-18 15:42:02 -0400

    Create top.php

---
+++ Blog/andrewjcole/top.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Blog - OpenSprites Resources</title>
+    <meta http-equiv="Content-Type" content="text/html; charset=ISO-8859-1">
+    <meta name="description" content="OpenSprites Resources. Free and open source resources for your Scratch projects">
+    <meta name="keywords" content="OpenSprites, opensprites, spritesopen, Opensprites, openSprites, OS, programming, resources, free, open source, open, resource, media, sprites, sounds, images, scripts, script, costume, backdrop, background">
+    <meta name="author" content="OpenSprites Team">
+    <meta name="subject" content="Website">
+    <link rel="stylesheet" type="text/css" href="main.css">
+    <link href='http://fonts.googleapis.com/css?family=Open+Sans:300' rel='stylesheet' type='text/css'>
+</head>
+
+<body>
+<noscript>Some features of this page may not work properly without javascript...</noscript>
+    <div id="header">
+        <h1 class="blog" align="center">BLOG</h1>
+        <span id="return"><a id="back" href="#" style="color:white">< Return to OpenSprites</a></span>
+    </div>
+<div id="content" align="left">
+<table style="width:100%">
+  <tr>
+    <td width="80%">    
