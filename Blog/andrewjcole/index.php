3677453c70f9de06085a136122ae3d7c75b2aa75
Andrew <andrewjcolescratch@gmail.com>
2015-02-18 15:14:06 -0400

    Create index.php

---
+++ Blog/andrewjcole/index.php
@@ -0,0 +1,59 @@
+<!DOCTYPE html>
+<html>
+<!-- DO NOT EDIT YET! I'm still working on this. -->
+<!-- Still need includes -->
+<head>
+    <title>Blog - OpenSprites Resources</title>
+    <meta http-equiv="Content-Type" content="text/html; charset=ISO-8859-1">
+    <meta name="description" content="OpenSprites Resources. Free and open source resources for your Scratch projects">
+    <meta name="keywords" content="OpenSprites, opensprites, spritesopen, Opensprites, openSprites, OS, programming, resources, free, open source, open, resource, media, sprites, sounds, images, scripts, script, costume, backdrop, background">
+    <meta name="author" content="OpenSprites Team">
+    <meta name="subject" content="Website">
+    <link rel="stylesheet" type="text/css" href="main.css">
+    <link href='http://fonts.googleapis.com/css?family=Open+Sans:300' rel='stylesheet' type='text/css'>
+</head>
+
+<body>
+<noscript>Some features of this page may not work properly without javascript...</noscript>
+    <div id="header">
+        <h1 class="blog" align="center">BLOG</h1>
+        <span id="return"><a id="back" href="#" style="color:white">< Return to OpenSprites</a></span>
+    </div>
+<div id="content" align="left">
+<table style="width:100%">
+  <tr>
+    <td width="80%">    
+        <!-- Article -->
+        <div id="article">
+
+        <h2 id="title">New features!</h2>
+        <p>Hello! Welcome to the blog! We hope you'll like it! Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+        <a href="#">Link</a>
+        
+        </div>
+        <!-- Article -->
+        <div id="article">
+
+        <h2 id="title">First Blog Post!</h2>
+        <p>Hello! Welcome to the blog! We hope you'll like it! Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+        
+        </div>
+    </td>
+    <!-- RIGHT SIDEBAR -->
+    <td class="sidebar" valign="top" align="center">
+        <h3>OpenSprites Blog</h3>
+            <p class="sidebar-text" align="left">&nbsp;&nbsp;&nbsp;&nbsp;Welcome to the OpenSprites Blog! This page is where you can find updates on OpenSprites as well as cool scripts and sprites highlighted by the admins!</p> 
+        <h3>Team</h3>
+            <img src="http://tinyurl.com/scratchericon">
+            <img src="http://tinyurl.com/scratchericon">
+            <img src="http://tinyurl.com/scratchericon">
+            <img src="http://tinyurl.com/scratchericon">
+    </td> 
+  </tr>
+</table>
+</div>
+<div id="footer">
+    <p align="center">2015 OpenSprites Team</p>
+</div>
+</body>
+</html>
