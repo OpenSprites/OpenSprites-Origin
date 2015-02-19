f1a44fd4be7fa524f88c3519fd7c9757b2e5db72
liam4 <liamdraws4@gmail.com>
2015-02-19 07:42:33 -0400

    Change to JS <script src> rather than PHP include

--- Blog/liam4/index.php
+++ Blog/liam4/index.php
@@ -3,6 +3,7 @@
     <head>
         <title></title>
         <?php include("header.php"); ?>
+        <script src="blogload.js" type="text/javascript"></script>
     </head>
     <body>
         <?php // This is slightly inspired by andrewjcole's blog, those who haven't should check it out at /Blog/andrewjcole but I'm not sure if it's been added to the main site yet :P ?>
