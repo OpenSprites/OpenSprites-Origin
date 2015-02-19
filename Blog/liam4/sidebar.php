12be91f9a94a9147fcd3ef344983be291466663d
liam4 <liamdraws4@gmail.com>
2015-02-19 10:44:36 -0400

    Changed liam4 blog sidebar to link to new locations
    Entries folder links to entries/index.php rather than entries/.
    OS links to http://dev.opensprites.x10.mx rather than /index.php.
    Blog Index renamed to My Blog Index and links to /liam4/index.php
    Added official blog index, links to /index.php.
    
    These changes were added because of the change to
    dev.opensprites.x10.mx, as well as bringing my blog to
    blog.opensprites.x10.mx/liam4.

--- Blog/liam4/sidebar.php
+++ Blog/liam4/sidebar.php
@@ -1,6 +1,7 @@
 <div id="sidebar">
     <h3>Sidebar</h3>
-    <a href="entries/">Entries folder</a>
-    <a href="/index.php">OpenSprites</a>
-    <a href="/Blog/liam4/index.php">Blog Index</a>
+    <a href="entries/index.php">Entries folder</a>
+    <a href="http://dev.opensprites.x10.mx">OpenSprites</a>
+    <a href="/liam4/index.php">My Blog Index</a>
+    <a href="/index.php">Official Blog Index</a>
 </div>
