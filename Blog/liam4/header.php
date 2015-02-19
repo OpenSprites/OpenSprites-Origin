a772224b125c5b6b4b2e0af0334645f7fb483d73
liam4 <liamdraws4@gmail.com>
2015-02-19 16:31:58 -0400

    Add styles for liam4 blog

--- Blog/liam4/header.php
+++ Blog/liam4/header.php
@@ -1,7 +1,6 @@
 <?php // Main header file containing scripts and stylesheets. ?>
 
-<link rel="stylesheet" href="global.css">
-<link rel="stylesheet" href="footer.css">
+<link rel="stylesheet" href="themes/<?php if ($_POST['theme']) {echo $_POST['theme'];} else {echo "beigedark";}?>/global.css">
 <script src="blogload.js" type="text/javascript"></script>
 <script src="jquery-1.10.2.min.js" type="text/javascript"></script>
-<script src="marked.min.js"></script>
+<script src="marked/marked.min.js"></script>
--- Blog/liam4/sidebar.php
+++ Blog/liam4/sidebar.php
@@ -4,4 +4,14 @@
     <a href="http://dev.opensprites.x10.mx">OpenSprites</a><br />
     <a href="/liam4/index.php">My Blog Index</a><br />
     <a href="/index.php">Official Blog Index</a><br />
+
+    <form action="index" method="POST" accept-charset="utf-8">
+        <select name="theme">
+            <option value="beigedark">Beige (Dark)</option>
+            <option value="beigelight">Beige (Light)</option>
+            <option value="blue">Blue</option>
+            <option value="red">Red</option>
+        </select>
+        <input type="submit" value="Change Theme">
+    </form>
 </div>
