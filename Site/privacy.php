5451e518d0e3d5ebb23ce7e41fe6123787efe0d6
DM <dom.m27@outlook.com>
2015-02-19 10:18:59 +0000

    Fixed navbar on privacy.php

--- Site/privacy.php
+++ Site/privacy.php
@@ -5,14 +5,14 @@
 <html>
 	<head>
 		<!--Imports the metadata and information that will go in the <head> of every page-->
-		<?php echo file_get_contents('Header.html'); ?>
+		<?php include('Header.php'); ?>
 	</head>
 	<body>
 		<!--Imports site-wide main styling-->
 		<link href='main-style.css' rel='stylesheet' type='text/css'>
 	
 		<!--Imports navigation bar-->
-		<?php include "navbar.php"; ?>
+		<?php include 'navbar.php'; ?>
 		
 		
 	</body>
