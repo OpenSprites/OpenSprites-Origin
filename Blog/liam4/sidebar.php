e06d07fc639f889d1257828d8764136c8e8e3602
liam4 <liamdraws4@gmail.com>
2015-02-19 10:55:00 -0400

    Added <br /> to liam4-blog sidebar
    Once again, an update to the sidebar. XD
    
    This time I added <br />’s to the end of each line, that should make it
    look much better.

--- Blog/liam4/sidebar.php
+++ Blog/liam4/sidebar.php
@@ -1,7 +1,7 @@
 <div id="sidebar">
     <h3>Sidebar</h3>
-    <a href="entries/index.php">Entries folder</a>
-    <a href="http://dev.opensprites.x10.mx">OpenSprites</a>
-    <a href="/liam4/index.php">My Blog Index</a>
-    <a href="/index.php">Official Blog Index</a>
+    <a href="entries/index.php">Entries folder</a><br />
+    <a href="http://dev.opensprites.x10.mx">OpenSprites</a><br />
+    <a href="/liam4/index.php">My Blog Index</a><br />
+    <a href="/index.php">Official Blog Index</a><br />
 </div>
