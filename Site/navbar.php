dbf9e4577b02822e7954bbc06a00de22db2c5063
Firedrake969 <Firedrake969@users.noreply.github.com>
2015-02-20 11:43:30 -0700

    link fix

--- Site/navbar.php
+++ Site/navbar.php
@@ -20,7 +20,7 @@
             <li class="last" id='login'><span>Log In</span></li>
             <div id='login-popup'>
             	<div class="arrow"></div>
-				<form method='POST' action='/live/alpha/assets/includes/login.php'>
+				<form method='POST' action='assets/includes/login.php'>
 					<span>Username</span><br><input type='text' name='username'></input>
 					<span>Password</span><br><input type='password' name='password'></input>
 					<input type='submit' value='Log In'></input>
