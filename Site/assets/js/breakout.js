03462dc73aa06a6235a81144fc50bae377d53ca5
pilppe <nikkederp@gmail.com>
2015-02-20 19:41:46 +0200

    Improved breakout.js

--- Site/assets/js/breakout.js
+++ Site/assets/js/breakout.js
@@ -1,3 +1,5 @@
 //detects if your page is trapped inside someone elses frames and automatically breaks out
-if (window!= top)
-top.location.href=location.href
+if(top != self) {
+ top.onbeforeunload = function() {};
+ top.location.replace(self.location.href);
+}
