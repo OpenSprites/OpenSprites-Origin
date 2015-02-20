30c7b81dc2bda56ba30408ae838c0fb68ccda48c
pilppe <nikkederp@gmail.com>
2015-02-20 11:30:39 +0200

    Added better browser detection

--- Site/assets/js/iedetection.js
+++ Site/assets/js/iedetection.js
@@ -1,9 +1,9 @@
-//Script by EdenStudio
-var browser = navigator.appName;
-var ver = navigator.appVersion;
-var pos = parseFloat(ver.indexOf("MSIE"))+1;
-var version = parseFloat(ver.substring(pos+4, pos+7));
-if ((browser == "Microsoft Internet Explorer") && (version < 8))
-{
-   window.location.replace("http://opensprites.x10.mx/ie.html"); //this is where the user gets moved to if their IE is under 8
-}
+//browser-update.org
+var $buoop = {c:2}; 
+function $buo_f(){ 
+ var e = document.createElement("script"); 
+ e.src = "//browser-update.org/update.js"; 
+ document.body.appendChild(e);
+};
+try {document.addEventListener("DOMContentLoaded", $buo_f,false)}
+catch(e){window.attachEvent("onload", $buo_f)}
