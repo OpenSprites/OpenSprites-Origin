909b2373b336c476a0a760292c81e75677f640ad
pilppe <nikkederp@gmail.com>
2015-02-21 19:25:42 +0200

    Deleted andrewjcoles blog

--- Blog/andrewjcole/README.txt
+++
@@ -1,1 +0,0 @@
-Okay, since you guys started adding stuff in there, I guess I'll add my ideas in here.
--- Blog/andrewjcole/bottom.php
+++
@@ -1,5 +0,0 @@
-<div id="footer">
-    <p align="center">2015 OpenSprites Team</p>
-</div>
-</body>
-</html>
