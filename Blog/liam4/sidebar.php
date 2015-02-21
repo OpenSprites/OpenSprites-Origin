af669975f06a256fb8148716d6afcb58695346cf
liam4 <liamdraws4@gmail.com>
2015-02-21 08:40:49 -0400

    Add archives link to blogoverview.php and organize CSS

--- Blog/liam4/sidebar.php
+++ Blog/liam4/sidebar.php
@@ -1,6 +1,6 @@
 <div id="sidebar">
     <h3>Sidebar</h3>
-    <a href="entries/index.php">Entries folder</a>
+    <a href="blogoverview.php">Archives</a>
     <a href="http://dev.opensprites.x10.mx">OpenSprites</a>
     <a href="/liam4/index.php">My Blog Index</a>
     <a href="/index.php">Official Blog Index</a>
