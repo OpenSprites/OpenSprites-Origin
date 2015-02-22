1ff0d2c1cdf55245371c71d645a43f5b37f20935
zappy1333 <zappythepro@gmail.com>
2015-02-22 10:06:36 +0000

    Add tipgiver

--- Site/index.php
+++ Site/index.php
@@ -90,6 +90,29 @@
 					</div>
 				</div>
 			</div>
+				<div id="feat-sprites">
+				<div class="box">
+					<h1>Top tips</h1>
+					<div class="box-content">
+					<p>
+					<?php
+					//Copy and paste this into another php file where you want the tip to be given.
+					$tips = array(
+					  "Remember to cap your scripts off with a stop this script block!",
+					  "OpenSprites was created by the OpenSprites team!",
+					  "Moving on from Scratch? Try Python!",
+					  "If you want to become a Scratcher, just be helpful and nice to other Scratchers!",
+					  "Have you got an account on OpenSprites yet?"
+					);
+					function giveTip()  {
+					return $tips[rand(0, tips.length)];
+					}
+					echo giveTip();
+					?>
+					</p>
+					</div>
+				</div>
+			</div>
 		</div>
 	</div>
     <!-- <div id="teams-favorite">
