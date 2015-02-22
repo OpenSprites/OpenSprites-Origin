796ba47ed46261f36c6c3c432a2a6bc5d8b769e5
Firedrake969 <firedrake969@users.noreply.github.com>
2015-02-22 12:57:50 -0700

    begin registration process

--- Site/register/register.php
+++ Site/register/register.php
@@ -1,1 +1,30 @@
-//EXISTS
+<?php
+	require '../assets/includes/connect.php';
+	
+	$username = mysqli_real_escape_string($connection, $_POST['username']);
+	$email = mysqli_real_escape_string($connection, $_POST['email']);
+	$password = mysqli_real_escape_string($connection, $_POST['password']);
+	$pass_conf = mysqli_real_escape_string($connection, $_POST['pass_conf']);
+	
+	$id = $_SESSION['login_code'];
+	
+	$good_username = false;
+	$good_password = false;
+	$good_email = false;
+	
+	if () {
+		$good_username = true;
+	}
+	
+	if () {
+		$good_password = true;
+	}
+	
+	if () {
+		$good_email = true;
+	}
+	
+	if ($good_email && $good_password && $good_username) {
+		//register...
+	}
+?>
