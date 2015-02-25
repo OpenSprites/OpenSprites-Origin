b3abbc44e5fbc371a2fd4f17e88c2da8fae0f9d8
Firedrake969 <firedrake969@users.noreply.github.com>
2015-02-24 17:10:00 -0700

    register.php
    Mostly copied from my (Firedrake969's) simple-php-login repo

--- Site/register/register.php
+++ Site/register/register.php
@@ -1,30 +1,57 @@
 <?php
 	require '../assets/includes/connect.php';
 	
-	$username = mysqli_real_escape_string($connection, $_POST['username']);
-	$email = mysqli_real_escape_string($connection, $_POST['email']);
-	$password = mysqli_real_escape_string($connection, $_POST['password']);
-	$pass_conf = mysqli_real_escape_string($connection, $_POST['pass_conf']);
+	$taken = FALSE;
+	$length = FALSE;
+	$goodEmail = FALSE;
+	$passCheck = FALSE;
+	$goodUsername = FALSE;
+	$maxUsernameLength = 50;
+	$minPasswordLength = 5;
 	
-	$id = $_SESSION['login_code'];
+	$is_bot = false;
 	
-	$good_username = false;
-	$good_password = false;
-	$good_email = false;
+	$min_time = 1.5
 	
-	if () {
-		$good_username = true;
+	if (empty($_POST)) {
+		$_SESSION['init_time'] = time();
+	} else {
+		//check if enough time has passed
+		$timediff = time() - $_SESSION['init_time'];
+		if ($timediff <= $min_time) {  //time diff is too small!
+			$is_bot = true;
+		}
 	}
 	
-	if () {
-		$good_password = true;
+	if (isset($_POST['username'], $_POST['password'], $_POST['password_check'], $_POST['email'])) {
+		$username = mysqli_real_escape_string($connection, $_POST['username']);
+		$query = "SELECT username FROM user_data WHERE username='$username'";
+		$result = mysqli_query($connection, $query);
+		if (mysqli_num_rows($result) > 0) {
+			$taken = TRUE;
+		}
+		if (filter_var($_POST['email'], FILTER_VALIDATE_EMAIL)) {
+			$goodEmail = TRUE;
+		}
+		if ($_POST['password'] == $_POST['password_check'] && strlen($_POST['password']) > $minPasswordLength) {
+			$passCheck = TRUE;
+		}
+		if (strlen($_POST['username']) <= $maxUsernameLength && preg_match('/^[a-zA-Z0-9_-]*$/', $username)) {
+			$goodUsername = TRUE;
+		}
+		if (!$taken && $goodEmail && $passCheck && $goodUsername && !$is_bot && isset($_SESSION['init_time'])) {
+			//all is good, so register!
+			$email = mysqli_real_escape_string($connection, $_POST['email']);
+			$hashedPassword = password_hash(mysqli_real_escape_string($connection, $_POST['password']), PASSWORD_DEFAULT);
+			$key = $username . hash("sha512", uniqid($username, true));
+			
+			//Can edit this to your preference.  Just keep the username, password, and user_key - those are the important ones!
+			$query = "
+				INSERT INTO user_data (username, password, email, user_key, date, ip, reg_key) 
+				VALUES ('$username', '$hashedPassword', '$email', '$key', CURDATE(), '" . $_SERVER['REMOTE_ADDR'] . "', '" . $_SESSION['user_code'] . "')
+			";
+			mysqli_query($connection, $query);
+		}
 	}
-	
-	if () {
-		$good_email = true;
-	}
-	
-	if ($good_email && $good_password && $good_username) {
-		//register...
-	}
+	header("Location: /");
 ?>
