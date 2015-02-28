40eb72daa2ffe210ca0708ca1d9c383563b6349b
Alex Bates <16batesa@gmail.com>
2015-02-28 13:15:48 +0000

    styling changes

--- Site/register/index.php
+++ Site/register/index.php
@@ -26,7 +26,7 @@
           <form enctype="multipart/form-data" action="register.php" method="POST">
             <div class="register" id="labels">Your Scratch Username:</div><input class="register" name="username" type="text" /><br>
 			<div class="register" id="labels">Email:</div><input class="register" name="email" type="email" /><br>
-            <div class="register" id="labels">Choose a Password (Please use a different password than your Scratch one):</div><input class="register" name="password" type="password" /><br>
+            <div class="register" id="labels">Choose a Password:<br>(Please use a different password than your Scratch one)</div><input class="register" name="password" type="password" /><br>
             <div class="register" id="labels">Confirm Password:</div><input class="register" name="confirm_password" type="password" /><br>
 			Please paste this code into <a href='http://scratch.mit.edu/projects/47606468/'>this</a> project:<br />
 			<?php
