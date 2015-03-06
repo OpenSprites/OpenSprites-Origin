25faf7dc33eca05291f1a63e2dfcb159ebaa2797
Alex Bates <16batesa@gmail.com>
2015-03-06 18:46:21 +0000

    Update and rename Site/themes to Site/themes/theme.js

---
+++ Site/themes/theme.js
@@ -0,0 +1,28 @@
+// helper functions
+Array.prototype.contains = function (needle) {
+	for (i in this) {
+		if (this[i] == needle) return true;
+	}
+	return false;
+}
+
+// when theme selection is changed
+$('#theme-select').on('change', function() {
+	var newt = $(this).val();
+	localStorage['os-theme'] = newt;
+	window.location.reload();
+});
+
+// is theme defined / valid?
+var themes = ['Regular', 'Dark'];
+if(typeof localStorage['os-theme'] === 'undefined' || !themes.contains(localStorage['os-theme'])) {
+	localStorage['os-theme'] = 'Regular';
+}
+
+// set selection to the saved theme
+$('#theme-select').val(localStorage['os-theme']);
+
+// set theme based on the theme selection
+var theme = $('#theme-select').val();
+$('head').append('<!-- theme = "' + theme + '" -->');
+$('head').append('<link href="' + theme + '/themes/.css" rel="stylesheet" type="text/css">');
