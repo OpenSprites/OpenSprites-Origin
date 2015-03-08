84bbbb2baaa63d3f998be4b9c884333bb34d2b51
GrannyCookies <16batesa@gmail.com>
2015-03-08 15:08:20 +0000

    updated emoticons plugin

--- forums/addons/plugins/Emoticons/plugin.php
+++ forums/addons/plugins/Emoticons/plugin.php
@@ -1,99 +1,144 @@
 <?php
-// Copyright 2011 Toby Zerner, Simon Zerner
-// This file is part of esoTalk. Please see the included license file for usage information.
 
 if (!defined("IN_ESOTALK")) exit;
 
 ET::$pluginInfo["Emoticons"] = array(
 	"name" => "Emoticons",
-	"description" => "Converts text emoticons to their graphical equivalent. Modified by the OpenSprites team.",
-	"version" => ESOTALK_VERSION,
-	"author" => "esoTalk Team / OpenSprites Team",
+	"description" => "Converts text emoticons to their graphical equivalent. And show add a button on formatting toolbar",
+	"version" => "1.0",
+	"author" => "Ramouch0 (based on esotalk's version)",
 	"authorEmail" => "support@esotalk.org",
 	"authorURL" => "http://esotalk.org",
 	"license" => "GPLv2"
 );
 
-class ETPlugin_Emoticons extends ETPlugin {
+class ETPlugin_EmoticonsPlus extends ETPlugin {
 
-public function handler_renderBefore($sender)
+private $icons;
+
+/**
+ * Class constructor.
+ *
+ * @return void
+ */
+public function __construct($rootDirectory)
 {
-	$sender->addCSSFile($this->resource("emoticons.css"));
+	parent::__construct($rootDirectory);
+	$this->icons = array();
+	$this->icons[":)"] = "background-position:0 0";
+	$this->icons[":-)"] = "background-position:0 0";
+	$this->icons["=)"] = "background-position:0 0";
+	$this->icons[":-D"] = "background-position:0 -20px";
+	$this->icons[":D"] = "background-position:0 -20px";
+	$this->icons["=D"] = "background-position:0 -20px";
+	$this->icons["^_^"] = "background-position:0 -40px";
+	$this->icons["^^"] = "background-position:0 -40px";
+	$this->icons[":("] = "background-position:0 -60px";
+	$this->icons[":-("] = "background-position:0 -60px";
+	$this->icons["=("] = "background-position:0 -60px";
+	$this->icons["-_-"] = "background-position:0 -80px";
+	$this->icons[";-)"] = "background-position:0 -100px";
+	$this->icons[";)"] = "background-position:0 -100px";
+	$this->icons["^_-"] = "background-position:0 -100px";
+	$this->icons["~_-"] = "background-position:0 -100px";
+	$this->icons["-_^"] = "background-position:0 -100px";
+	$this->icons["-_~"] = "background-position:0 -100px";
+	$this->icons["^_^;"] = "background-position:0 -120px; width:18px";
+	$this->icons["^^;"] = "background-position:0 -120px; width:18px";
+	$this->icons[">_<"] = "background-position:0 -140px";
+	$this->icons[":/"] = "background-position:0 -160px";
+	$this->icons[":-/"] = "background-position:0 -160px";
+	$this->icons["=/"] = "background-position:0 -160px";
+	$this->icons[":\\"] = "background-position:0 -160px";
+	$this->icons["=\\"] = "background-position:0 -160px";
+	$this->icons[":x"] = "background-position:0 -180px";
+	$this->icons["=x"] = "background-position:0 -180px";
+	$this->icons[":|"] = "background-position:0 -180px";
+	$this->icons["=|"] = "background-position:0 -180px";
+	$this->icons["'_'"] = "background-position:0 -180px";
+	$this->icons["<_<"] = "background-position:0 -200px";
+	$this->icons[">_>"] = "background-position:0 -220px";
+	$this->icons["x_x"] = "background-position:0 -240px";
+	$this->icons["o_O"] = "background-position:0 -260px";
+	$this->icons["O_o"] = "background-position:0 -260px";
+	$this->icons["o_0"] = "background-position:0 -260px";
+	$this->icons["0_o"] = "background-position:0 -260px";
+	$this->icons[";_;"] = "background-position:0 -280px";
+	$this->icons[":'("] = "background-position:0 -280px";
+	$this->icons[":O"] = "background-position:0 -300px";
+	$this->icons["=O"] = "background-position:0 -300px";
+	$this->icons[":o"] = "background-position:0 -300px";
+	$this->icons["=o"] = "background-position:0 -300px";
+	$this->icons[":-P"] = "background-position:0 -320px";
+	$this->icons[":P"] = "background-position:0 -320px";
+	$this->icons[":p"] = "background-position:0 -320px";
+	$this->icons[":-p"] = "background-position:0 -320px";
+	$this->icons["=P"] = "background-position:0 -320px";
+	$this->icons[";P"] = "background-position:0 -320px";
+	$this->icons[";-P"] = "background-position:0 -320px";
+	$this->icons[":["] = "background-position:0 -340px";
+	$this->icons["=["] = "background-position:0 -340px";
+	$this->icons[":3"] = "background-position:0 -360px";
+	$this->icons["=3"] = "background-position:0 -360px";
+	$this->icons["._.;"] = "background-position:0 -380px; width:18px";
+	$this->icons["<(^.^)>"] = "background-position:0 -400px; width:19px";
+	$this->icons["(>'.')>"] = "background-position:0 -400px; width:19px";
+	$this->icons["(>^.^)>"] = "background-position:0 -400px; width:19px";
+	$this->icons["-_-;"] = "background-position:0 -420px; width:18px";
+	$this->icons["(o^_^o)"] = "background-position:0 -440px";
+	$this->icons["(^_^)/"] = "background-position:0 -460px; width:19px";
+	$this->icons[">:("] = "background-position:0 -480px";
+	$this->icons[">:["] = "background-position:0 -480px";
+	$this->icons["._."] = "background-position:0 -500px";
+	$this->icons["T_T"] = "background-position:0 -520px";
+	$this->icons["XD"] = "background-position:0 -540px";
+	$this->icons["('<"] = "background-position:0 -560px";
+	$this->icons["B)"] = "background-position:0 -580px";
+	$this->icons["XP"] = "background-position:0 -600px";
+	$this->icons[":S"] = "background-position:0 -620px";
+	$this->icons["=S"] = "background-position:0 -620px";
+	$this->icons[">:)"] = "background-position:0 -640px";
+	$this->icons[">:D"] = "background-position:0 -640px";
+}
+
+/**
+ * Add an event handler to the "getEditControls" method of the conversation controller to add BBCode
+ * formatting buttons to the edit controls.
+ *
+ * @return void
+ */
+public function handler_conversationController_getEditControls($sender, &$controls, $id)
+{
+
+	addToArrayString($controls, "smileys", "<a href='javascript:EmoticonAdv.showDropDown(\"$id\");void(0)' title='".T("Smileys")."' class='emoticon'><span>".T("Smileys")."</span></a>", 0);
+}
+
+public function handler_conversationController_renderBefore($sender)
+{
+	$sender->addJSFile($this->getResource("emoticon.js"));
+	$sender->addCSSFile($this->getResource("emoticon.css"));
+}
+
+public function handler_memberController_renderBefore($sender)
+{
+	$this->handler_conversationController_renderBefore($sender);
+}
+
+public function handler_pageEnd(){
+
+	$div ="<div id='emoticonDropDown' style='display: none;'><div><ul>";
+	foreach(array_unique($this->icons) as $k => $v){
+		$alt = htmlentities($k, ENT_QUOTES);
+		$div.="<li><a href=\"javascript:EmoticonAdv.insertSmiley('".str_replace("'","\'",$k)."');void(0)\" title=\"$alt\" style=\"$v\" class=\"emoticon\" alt=\"$alt\">\"$alt\"</a></li>";
+	}
+	$div .="</ul></div></div>";
+	echo $div;
 }
 
 public function handler_format_format($sender)
 {
-	if ($sender->inline) return;
-
-	$styles = array();
-	$styles[":)"] = "background-position:0 0";
-	$styles["=)"] = "background-position:0 0";
-	$styles[":D"] = "background-position:0 -20px";
-	$styles["=D"] = "background-position:0 -20px";
-	$styles["^_^"] = "background-position:0 -40px";
-	$styles["^^"] = "background-position:0 -40px";
-	$styles[":("] = "background-position:0 -60px";
-	$styles["=("] = "background-position:0 -60px";
-	$styles["-_-"] = "background-position:0 -80px";
-	$styles[";)"] = "background-position:0 -100px";
-	$styles["^_-"] = "background-position:0 -100px";
-	$styles["~_-"] = "background-position:0 -100px";
-	$styles["-_^"] = "background-position:0 -100px";
-	$styles["-_~"] = "background-position:0 -100px";
-	$styles["^_^;"] = "background-position:0 -120px; width:18px";
-	$styles["^^;"] = "background-position:0 -120px; width:18px";
-	$styles[">_<"] = "background-position:0 -140px";
-	$styles[":/"] = "background-position:0 -160px";
-	$styles["=/"] = "background-position:0 -160px";
-	$styles[":\\"] = "background-position:0 -160px";
-	$styles["=\\"] = "background-position:0 -160px";
-	$styles[":x"] = "background-position:0 -180px";
-	$styles["=x"] = "background-position:0 -180px";
-	$styles[":|"] = "background-position:0 -180px";
-	$styles["=|"] = "background-position:0 -180px";
-	$styles["'_'"] = "background-position:0 -180px";
-	$styles["<_<"] = "background-position:0 -200px";
-	$styles[">_>"] = "background-position:0 -220px";
-	$styles["x_x"] = "background-position:0 -240px";
-	$styles["o_O"] = "background-position:0 -260px";
-	$styles["O_o"] = "background-position:0 -260px";
-	$styles["o_0"] = "background-position:0 -260px";
-	$styles["0_o"] = "background-position:0 -260px";
-	$styles[";_;"] = "background-position:0 -280px";
-	$styles[":'("] = "background-position:0 -280px";
-	$styles[":O"] = "background-position:0 -300px";
-	$styles["=O"] = "background-position:0 -300px";
-	$styles[":o"] = "background-position:0 -300px";
-	$styles["=o"] = "background-position:0 -300px";
-	$styles[":P"] = "background-position:0 -320px";
-	$styles["=P"] = "background-position:0 -320px";
-	$styles[";P"] = "background-position:0 -320px";
-	$styles[":["] = "background-position:0 -340px";
-	$styles["=["] = "background-position:0 -340px";
-	$styles[":3"] = "background-position:0 -360px";
-	$styles["=3"] = "background-position:0 -360px";
-	$styles["._.;"] = "background-position:0 -380px; width:18px";
-	$styles["<(^.^)>"] = "background-position:0 -400px; width:19px";
-	$styles["(>'.')>"] = "background-position:0 -400px; width:19px";
-	$styles["(>^.^)>"] = "background-position:0 -400px; width:19px";
-	$styles["-_-;"] = "background-position:0 -420px; width:18px";
-	$styles["(o^_^o)"] = "background-position:0 -440px";
-	$styles["(^_^)/"] = "background-position:0 -460px; width:19px";
-	$styles[">:("] = "background-position:0 -480px";
-	$styles[">:["] = "background-position:0 -480px";
-	$styles["._."] = "background-position:0 -500px";
-	$styles["T_T"] = "background-position:0 -520px";
-	$styles["XD"] = "background-position:0 -540px";
-	$styles["('<"] = "background-position:0 -560px";
-	$styles["B)"] = "background-position:0 -580px";
-	$styles["XP"] = "background-position:0 -600px";
-	$styles[":S"] = "background-position:0 -620px";
-	$styles["=S"] = "background-position:0 -620px";
-	$styles[">:)"] = "background-position:0 -640px";
-	$styles[">:D"] = "background-position:0 -640px";
-
 	$from = $to = array();
-	foreach ($styles as $k => $v) {
+	foreach ($this->icons as $k => $v) {
 		$quoted = preg_quote(sanitizeHTML($k), "/");
 		$from[] = "/(?<=^|[\s.,!<>]){$quoted}(?=[\s.,!<>)]|$)/i";
 		$to[] = "<span class='emoticon' style='$v'>$k</span>";
