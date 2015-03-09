33d32512f4508d452cd9adee6d94308f83845e28
Alex Bates <16batesa@gmail.com>
2015-03-09 20:35:22 +0000

    Update connect.php

--- Site/assets/includes/connect.php
+++ Site/assets/includes/connect.php
@@ -3,6 +3,11 @@
   session_start();
   
   require "../assets/includes/html_dom_parser.php";
-  $logged_in_userid = $_SESSION["userId"];
-  $html = file_get_html('http://opensprites.x10.mx/forums/?p=member/' . $logged_in_userid);
-  $logged_in_user =   $html->find('h1#memberName', 0)->innertext;
+  if(isset($_SESSION["userId"])) {
+    $logged_in_userid = $_SESSION["userId"];
+    $html = file_get_html('http://opensprites.x10.mx/forums/?p=member/' . $logged_in_userid);
+    $logged_in_user = $html->find('h1#memberName', 0)->innertext;
+  } else {
+    $logged_in_userid = 'not logged in';
+    $logged_in_user = 'not logged in';
+  }
