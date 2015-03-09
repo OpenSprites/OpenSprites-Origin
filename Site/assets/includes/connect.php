4956619fd99c6515bffdb8a11f3519aba6fc0417
Alex Bates <16batesa@gmail.com>
2015-03-09 20:28:49 +0000

    Update connect.php

--- Site/assets/includes/connect.php
+++ Site/assets/includes/connect.php
@@ -2,7 +2,7 @@
   session_name("OpenSprites_Forum_session");
   session_start();
   
-  include 'http://dev.opensprites.x10.mx/assets/includes/html_dom_parser.php';
+  require "../assets/includes/html_dom_parser.php";
   $logged_in_userid = $_SESSION["userId"];
   $html = file_get_html('http://opensprites.x10.mx/forums/?p=member/' . $id);
   $logged_in_user =   $html->find('h1#memberName', 0)->innertext;
