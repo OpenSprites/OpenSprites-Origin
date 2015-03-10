8d2dec71633adcca418fee7845839a9288a3d17e
Alex Bates <16batesa@gmail.com>
2015-03-10 18:53:27 +0000

    Update navbar.php

--- Site/navbar.php
+++ Site/navbar.php
@@ -19,15 +19,15 @@
         <?php if($logged_in_user == 'not logged in') { ?>
             <li><a href="register">Join OpenSprites!</a>
             </li>
-            <li class="last" id='login'><span>Log In</span></li>
-            <div id='login-popup'>
+            <li class="last" id='login' onlick="window.location = 'http://opensprites.x10.mx/forums/?p=user/login&return=' + window.location.href;"><span>Log In</span></li>
+            <!--<div id='login-popup'>
             	<div class="arrow"></div>
 				<form method='POST' action='assets/includes/login.php'>
 					<span>Username</span><br><input type='text' name='username'></input>
 					<span>Password</span><br><input type='password' name='password'></input>
 					<input type='submit' value='Log In'></input>
 				</form>
-			</div>
+			</div>-->
             
         <?php } else  { ?>  <!-- display login info/username/etc -->
 			<div id='login-display'><?php echo $logged_in_user; ?></div>
