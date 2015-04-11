4ceea76d1cb4c3fe2b108ca45ffd455db6f4b2f1
GrannyCookies <16batesa@gmail.com>
2015-04-11 11:34:42 +0100

    fancy 404 page

--- Site/index.php
+++ Site/index.php
@@ -1,94 +1,94 @@
 <?php
-	require "assets/includes/connect.php";  //Connect - includes session_start();
+    require "assets/includes/connect.php";  //Connect - includes session_start();
 ?>
 <!DOCTYPE html>
 <html>
 <head>
-	<!--Imports the metadata and information that will go in the <head> of every page-->
-	<?php include 'Header.html'; ?>
+    <!--Imports the metadata and information that will go in the <head> of every page-->
+    <?php include 'Header.html'; ?>
 </head>
 <body>
-	<!--Imports site-wide main styling-->
-	<link href='main-style.css' rel='stylesheet' type='text/css'>
-	
-	<!--Imports navigation bar-->
-	<?php include "navbar.php"; ?>
-	
-	<!-- Main wrapper -->
-	<div class="container main">
-		<div class="main-inner">
-			<h1 id="opensprites-heading">OpenSprites - Share Sprites, Scripts, and More!</h1>
-			<div id="about">
-				<h2>Welcome!</h2>
-				<p>Welcome to OpenSprites, the free, open-source site that allows <a href='//scratch.mit.edu'>Scratch</a> users to share their own scripts, sprites and project media!</p>
-			</div>
-			<div id="top-sprites">
-				<div class="box">
-					<h1>Top Sprites</h1>
-					<div class="box-content">
-						<p>Sort by: 
-							<select id="sortby">
-								<option>Popularity (downloads)</option>
-								<option>Ratings</option>
-								<option>A-Z</option>
-								<option>Newest</option>
-								<option>Oldest</option>
-							</select>
-						</p>
-						PHP guys, get in here!
-					</div>
-				</div>
-			</div>
-			<div id="top-scripts">
-				<div class="box">
-					<h1>Top Scripts</h1>
-					<div class="box-content">
-						<p>Sort by: 
-							<select id="sortby">
-								<option>Popularity (downloads)</option>
-								<option>Ratings</option>
-								<option>A-Z</option>
-								<option>Newest</option>
-								<option>Oldest</option>
-							</select>
-						</p>
-						PHP guys, get in here!
-					</div>
-				</div>
-			</div>
-			<div id="feat-sprites">
-				<div class="box">
-					<h1>Featured Sprites</h1>
-					<div class="box-content">
-						<p>Sort by: 
-							<select id="sortby">
-								<option>Popularity (downloads)</option>
-								<option>Ratings</option>
-								<option>A-Z</option>
-								<option>Newest</option>
-								<option>Oldest</option>
-							</select>
-						</p>
-						PHP guys, get in here!
-					</div>
-				</div>
-			</div>
-		</div>
-	</div>
+    <!--Imports site-wide main styling-->
+    <link href='main-style.css' rel='stylesheet' type='text/css'>
+    
+    <!--Imports navigation bar-->
+    <?php include "navbar.php"; ?>
+    
+    <!-- Main wrapper -->
+    <div class="container main">
+        <div class="main-inner">
+            <h1 id="opensprites-heading">OpenSprites - Share Sprites, Scripts, and More!</h1>
+            <div id="about">
+                <h2>Welcome!</h2>
+                <p>Welcome to OpenSprites, the free, open-source site that allows <a href='//scratch.mit.edu'>Scratch</a> users to share their own scripts, sprites and project media!</p>
+            </div>
+            <div id="top-sprites">
+                <div class="box">
+                    <h1>Top Sprites</h1>
+                    <div class="box-content">
+                        <p>Sort by:
+                            <select id="sortby">
+                                <option>Popularity (downloads)</option>
+                                <option>Ratings</option>
+                                <option>A-Z</option>
+                                <option>Newest</option>
+                                <option>Oldest</option>
+                            </select>
+                        </p>
+                        PHP guys, get in here!
+                    </div>
+                </div>
+            </div>
+            <div id="top-scripts">
+                <div class="box">
+                    <h1>Top Scripts</h1>
+                    <div class="box-content">
+                        <p>Sort by:
+                            <select id="sortby">
+                                <option>Popularity (downloads)</option>
+                                <option>Ratings</option>
+                                <option>A-Z</option>
+                                <option>Newest</option>
+                                <option>Oldest</option>
+                            </select>
+                        </p>
+                        PHP guys, get in here!
+                    </div>
+                </div>
+            </div>
+            <div id="feat-sprites">
+                <div class="box">
+                    <h1>Featured Sprites</h1>
+                    <div class="box-content">
+                        <p>Sort by:
+                            <select id="sortby">
+                                <option>Popularity (downloads)</option>
+                                <option>Ratings</option>
+                                <option>A-Z</option>
+                                <option>Newest</option>
+                                <option>Oldest</option>
+                            </select>
+                        </p>
+                        PHP guys, get in here!
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
     <!-- <div id="teams-favorite">
            <div class="box">
                <h1>Featured Scripts and Sprites</h1>
-				   <div class="box-content">
-						<p>The OS Team will choose two scripts and two sprites daily.
-						</p>
-						PHP guys, get in here!
-					</div>
-				</div>
-			</div>
-		</div>
-	</div> -->
-	
-	<!-- footer -->
-	<?php echo file_get_contents('footer.html'); ?>
+                   <div class="box-content">
+                        <p>The OS Team will choose two scripts and two sprites daily.
+                        </p>
+                        PHP guys, get in here!
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div> -->
+    
+    <!-- footer -->
+    <?php echo file_get_contents('footer.html'); ?>
 </body>
 </html>
--- Site/upload/index.php
+++ Site/upload/index.php
@@ -1,15 +1,15 @@
 <?php
-	require "../assets/includes/connect.php";  //Connect - includes session_start();
-	require "../assets/includes/avatar.php";
+    require "../assets/includes/connect.php";  //Connect - includes session_start();
+    require "../assets/includes/avatar.php";
 ?>
 <!DOCTYPE html>
 <html>
 <head>
-	<!--Imports the metadata and information that will go in the <head> of every page-->
-	<?php echo file_get_contents('../Header.html'); ?>
-	
-	<!--Imports styling-->
-	<link href='style.css' rel='stylesheet' type='text/css'>
+    <!--Imports the metadata and information that will go in the <head> of every page-->
+    <?php echo file_get_contents('../Header.html'); ?>
+    
+    <!--Imports styling-->
+    <link href='style.css' rel='stylesheet' type='text/css'>
 </head>
 <body>
     <script>
@@ -24,45 +24,45 @@
             $('.main').fadeIn(250);
         };
     </script>
-	<!--Imports navigation bar-->
-	<?php include "../navbar.php"; ?>
-	
-	<!-- Select Method -->
-	<?php if(!isset($_GET['method'])) { ?>
+    <!--Imports navigation bar-->
+    <?php include "../navbar.php"; ?>
+    
+    <!-- Select Method -->
+    <?php if(!isset($_GET['method'])) { ?>
     <div class="container main" style="text-align:center;display:none;">
-		<div class="main-inner" style="">
-		<h1 style="font-size:4em;margin-top:50px;">Upload</h1>
-		<h1 style="font-size:3em;margin-top:10px;">Choose an upload method</h1>
-		<img src="fromLocal.png" class="method" onclick="selectMethod('local');"><img src="fromScratch.png" class="method" onclick="selectMethod('scratch');">
-		<div style="height:100px;width:100%;"><!-- moves navbar down --></div>
-		</div>
-	</div>
-	<?php } else if($_GET['method'] == 'local') { ?>
-	
-	<!-- From Local File -->
+        <div class="main-inner" style="">
+        <h1 style="font-size:4em;margin-top:50px;">Upload</h1>
+        <h1 style="font-size:3em;margin-top:10px;">Choose an upload method</h1>
+        <img src="fromLocal.png" class="method" onclick="selectMethod('local');"><img src="fromScratch.png" class="method" onclick="selectMethod('scratch');">
+        <div style="height:100px;width:100%;"><!-- moves navbar down --></div>
+        </div>
+    </div>
+    <?php } else if($_GET['method'] == 'local') { ?>
+    
+    <!-- From Local File -->
     <div class="container main" style="text-align:center;display:none;">
-		<div class="main-inner" style="">
-			<h1 style="font-size:4em;margin-top:50px;">Upload</h1>
-			<h1 style="font-size:3em;margin-top:10px;">Select a file</h1>
-		</div>
-	</div>
-	<?php } else if($_GET['method'] == 'scratch') { ?>
-	
-	<!-- From Scratch -->
-	<div class="container main" style="text-align:center;display:none;">
-		<div class="main-inner" style="">
-			<h1 style="font-size:4em;margin-top:50px;">Upload</h1>
-			<h1 style="font-size:3em;margin-top:10px;">Select a Scratch project</h1>
-		</div>
-	</div>
-	
-	<?php
+        <div class="main-inner" style="">
+            <h1 style="font-size:4em;margin-top:50px;">Upload</h1>
+            <h1 style="font-size:3em;margin-top:10px;">Select a file</h1>
+        </div>
+    </div>
+    <?php } else if($_GET['method'] == 'scratch') { ?>
+    
+    <!-- From Scratch -->
+    <div class="container main" style="text-align:center;display:none;">
+        <div class="main-inner" style="">
+            <h1 style="font-size:4em;margin-top:50px;">Upload</h1>
+            <h1 style="font-size:3em;margin-top:10px;">Select a Scratch project</h1>
+        </div>
+    </div>
+    
+    <?php
     } else {
         header('location: /upload/');
     }
     ?>
-	
-	<!-- footer -->
-	<?php echo file_get_contents('../footer.html'); ?>
+    
+    <!-- footer -->
+    <?php echo file_get_contents('../footer.html'); ?>
 </body>
 </html>
