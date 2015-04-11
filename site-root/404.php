28452abf6866abc278c51c8aa6b61c49f29c5763
cheeseeater <admin@cheeseeater.t15.org>
2015-04-11 18:00:46 +0100

    Site root

---
+++ site-root/404.php
@@ -0,0 +1,32 @@
+<?php
+    require "assets/includes/connect.php";  //Connect - includes session_start();
+?>
+<!DOCTYPE html>
+<html>
+<head>
+    <!--Imports the metadata and information that will go in the <head> of every page-->
+    <?php include 'Header.html'; ?>
+</head>
+<body>
+    <!--Imports site-wide main styling-->
+    <link href='main-style.css' rel='stylesheet' type='text/css'>
+    
+    <!--Imports navigation bar-->
+    <?php include "navbar.php"; ?>
+    
+    <!-- Main wrapper -->
+    <div class="container main">
+        <div class="main-inner">
+            <h1 id="opensprites-heading">Our server is a little confused...</h1>
+            <div id="about">
+                <img src='assets/images/404.png' style='position: absolute; margin: auto; left: 0; right: 0;'>
+                <div style='width: 100%; height: 400px;'>&nbsp;</div>
+                <p style='position: absolute; margin: auto; top: 480px; left: 0; right: 0; width: 50%; text-align: center; font-size: 18px;'>We couldn't find the page you're looking for.<br>You may want to <a href='/'>go back to the main page</a>.</p>
+            </div>
+        </div>
+    </div>
+    
+    <!-- footer -->
+    <?php echo file_get_contents('footer.html'); ?>
+</body>
+</html>
