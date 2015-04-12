fef4cb448ca4813e0fa7e808bfbc03aee74a98db
Alex Bates <16batesa@gmail.com>
2015-04-12 15:04:51 +0100

    SEAMLESS REGISTER

--- Site/register/index.php
+++ Site/register/index.php
@@ -1,3 +1,31 @@
 <?php
-header('Location: http://opensprites.gwiddle.co.uk/forums/?p=user/join&return=http://dev.opensprites.gwiddle.co.uk');
+    require $_SERVER['DOCUMENT_ROOT']."/assets/includes/connect.php";  //Connect - includes session_start();
+    if($logged_in_user !== 'not logged in') {
+        header('Location: /');
+    }
 ?>
+<!DOCTYPE html>
+<html>
+<head>
+    <!--Imports the metadata and information that will go in the <head> of every page-->
+    <?php include $_SERVER['DOCUMENT_ROOT'].'/Header.html'; ?>
+</head>
+<body>
+    <!--Imports site-wide main styling-->
+    <link href='/main-style.css' rel='stylesheet' type='text/css'>
+    
+    <!--Imports navigation bar-->
+    <?php include $_SERVER['DOCUMENT_ROOT']."/navbar.php"; ?>
+    
+    <!-- Main wrapper -->
+    <div class="container main" style="height:700px;">
+        <div class="main-inner">
+            <div style="position: absolute;left: 0;right: 0;height: 70px;" class="themebg"></div>
+            <iframe scrolling="no" src="http://opensprites.gwiddle.co.uk/forums/?p=user/register&return=http://dev.opensprites.gwiddle.co.uk/&iframe=true" style="width: 100%; height: 360px;overflow: hidden;border: none;"></iframe>
+        </div>
+    </div>
+    
+    <!-- footer -->
+    <?php echo file_get_contents($_SERVER['DOCUMENT_ROOT'].'/footer.html'); ?>
+</body>
+</html>
