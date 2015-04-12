7e59565ee4e9eadd39228e32e10afd01c2e0722b
Alex Bates <16batesa@gmail.com>
2015-04-12 15:08:19 +0100

    SEAMLESS REGISTER
    fix #2

--- Site/register/index.php
+++ Site/register/index.php
@@ -21,7 +21,7 @@
     <div class="container main" style="height:700px;">
         <div class="main-inner">
             <div style="position: absolute;left: 0;right: 0;height: 70px;" class="themebg"></div>
-            <iframe scrolling="no" src="http://opensprites.gwiddle.co.uk/forums/?p=user/join&return=http://dev.opensprites.gwiddle.co.uk/&iframe=true" style="width: 100%; height: 360px;overflow: hidden;border: none;"></iframe>
+            <iframe scrolling="no" src="http://opensprites.gwiddle.co.uk/forums/?p=user/join&return=http://dev.opensprites.gwiddle.co.uk/&iframe=true" style="width: 100%; height: 570px;overflow: hidden;border: none;"></iframe>
         </div>
     </div>
     
