5037c1796db93e1ade7d7260311d021aaf888473
GrannyCookies <16batesa@gmail.com>
2015-04-12 18:11:52 +0100

    Cookie Consent for EU Viewers
    According to the EU Law, the end-user MUST have given consent to the use
    of cookies. So here it is.

---
+++ Site/assets/js/cookieconsent.js
@@ -0,0 +1,10 @@
+(function () {
+    $(function () {
+        if(sGeobytesMapReference === 'Europe ' && localStorage['cookies'] === undefined) {
+            // Europe Cookie Consent Law
+            $('head').append('<style>#cookielaw-text {position:absolute;left:40px;font-size:21px;height:27px;line-height:24px;} #cookielaw {position:fixed;bottom:0;right:0;width:100%;font-size:18px;line-height:18px;padding:12px;background:rgb(174,207,54);color:black;}</style>');
+            
+            $('body').prepend('<div id="cookielaw"><div id="cookielaw-text">We use cookies on this site. By using this site or closing this message, you are allowing us to use cookies.</div><a href="javascript:$(\'#cookielaw\').fadeOut(300);localStorage[\'cookies\'] = true;" class="btn blue" style="float: right; width: 100px; text-align:center;">Close</a></div>');
+        }
+    });
+})();
