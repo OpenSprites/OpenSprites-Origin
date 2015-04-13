271ba7e1ac2087e18488c15a59083bb28cbc2985
MegaApuTurkUltra <apu@aputurk.tk>
2015-04-13 17:48:58 -0400

    Added stuff on upload pages

--- Site/upload/index.php
+++ Site/upload/index.php
@@ -14,53 +14,56 @@
 <body>
     <script>
         function selectMethod(type) {
-            $('.main').fadeOut(250);
-            setTimeout(function() {
-                window.location = '?method=' + type;
-            }, 250);
+            window.location = '?method=' + type; // timeout made it look bad
+			// if you really want it to look nice, you should use ajax
         }
-        
-        window.onload = function() {
-            $('.main').fadeIn(250);
-        };
     </script>
     <!--Imports navigation bar-->
     <?php include "../navbar.php"; ?>
     
+	<div class="container main" style="text-align:center;">
+    <div class="main-inner">
+		
     <!-- Select Method -->
     <?php if(!isset($_GET['method'])) { ?>
-    <div class="container main" style="text-align:center;display:none;">
-        <div class="main-inner" style="">
         <h1 style="font-size:4em;margin-top:50px;">Upload</h1>
         <h1 style="font-size:3em;margin-top:10px;">Choose an upload method</h1>
-        <img src="fromLocal.png" class="method" onclick="selectMethod('local');"><img src="fromScratch.png" class="method" onclick="selectMethod('scratch');">
-        <div style="height:100px;width:100%;"><!-- moves navbar down --></div>
-        </div>
-    </div>
+		<div id='upload-method-select'>
+			<img src="/assets/images/upload/fromLocal.png" class="method" onclick="selectMethod('local');">
+			<img src="/assets/images/upload/fromScratch.png" class="method" onclick="selectMethod('scratch');">
+			<img src="/assets/images/upload/fromUrl.png" class="method" onclick="selectMethod('url');">
+		</div>
     <?php } else if($_GET['method'] == 'local') { ?>
     
     <!-- From Local File -->
-    <div class="container main" style="text-align:center;display:none;">
-        <div class="main-inner" style="">
             <h1 style="font-size:4em;margin-top:50px;">Upload</h1>
             <h1 style="font-size:3em;margin-top:10px;">Select a file</h1>
-        </div>
-    </div>
     <?php } else if($_GET['method'] == 'scratch') { ?>
     
     <!-- From Scratch -->
-    <div class="container main" style="text-align:center;display:none;">
-        <div class="main-inner" style="">
             <h1 style="font-size:4em;margin-top:50px;">Upload</h1>
             <h1 style="font-size:3em;margin-top:10px;">Select a Scratch project</h1>
-        </div>
-    </div>
+	
+	<?php } else if($_GET['method'] == 'url') { ?>
     
+	<!-- From URL -->
+            <h1 style="font-size:4em;margin-top:50px;">Upload</h1>
+            <h1 style="font-size:3em;margin-top:10px;">Enter a URL</h1>
+	
     <?php
     } else {
         header('location: /upload/');
     }
     ?>
+	
+	<?php if(isset($_GET['method'])){ ?>
+	
+		<p><a href="/upload/">Back</a></p>
+	
+	<?php } ?>
+	
+	</div>
+    </div>
     
     <!-- footer -->
     <?php echo file_get_contents('../footer.html'); ?>
