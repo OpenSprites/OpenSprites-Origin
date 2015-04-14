f476435146832b387813a1e4001b21a99c31c6df
Liam (Aqua) <liam4@users.noreply.github.com>
2015-04-14 06:53:57 -0300

    Create 403.php
    For those who just don't like the "forbidden" default page.

---
+++ Site/403.php
@@ -0,0 +1,32 @@
+<?php
+    require $_SERVER['DOCUMENT_ROOT']."/assets/includes/connect.php";  //Connect - includes session_start();
+?>
+<!DOCTYPE html>
+<html>
+<head>
+    <!--Imports the metadata and information that will go in the <head> of every page-->
+    <?php include $_SERVER['DOCUMENT_ROOT'].'/Header.html'; ?>
+</head>
+<body>
+    <!--Imports site-wide main styling-->
+    <link href='/main-style.css' rel='stylesheet' type='text/css'>
+    
+    <!--Imports navigation bar-->
+    <?php include $_SERVER['DOCUMENT_ROOT']."/navbar.php"; ?>
+    
+    <!-- Main wrapper -->
+    <div class="container main">
+        <div class="main-inner">
+            <h1 id="opensprites-heading">Did you go to the wrong place..?</h1>
+            <div id="about">
+                <img src='assets/images/404.png' style='position: absolute; margin: auto; left: 0; right: 0;'>
+                <div style='width: 100%; height: 470px;'>&nbsp;</div>
+                <p style='position: absolute; margin: auto; top: 480px; left: 0; right: 0; width: 50%; text-align: center; font-size: 18px;'>It looks like you don't have permission to go here.<br>You may want to <a href='/'>go back to the main page</a>.</p>
+            </div>
+        </div>
+    </div>
+    
+    <!-- footer -->
+    <?php echo file_get_contents($_SERVER['DOCUMENT_ROOT'].'/footer.html'); ?>
+</body>
+</html>
