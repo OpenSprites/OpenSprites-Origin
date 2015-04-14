17a7ac8ed1f64c0c6df0b2c5bcf17d0ddfb957bc
Alex Bates <16batesa@gmail.com>
2015-04-14 16:49:15 +0100

    fixed php tags

--- Site/navbar.php
+++ Site/navbar.php
@@ -6,8 +6,8 @@
     // let's try to merge to using proper JS technique. Remember, we want this code to be maintainable.
     var OpenSprites = OpenSprites || {};
     OpenSprites.user = {};
-    OpenSprites.user.name = <? php echo json_encode($logged_in_user); ?> ;
-    OpenSprites.user.id = <? php echo json_encode($logged_in_userid); ?> ;
+    OpenSprites.user.name = <?php echo json_encode($logged_in_user); ?> ;
+    OpenSprites.user.id = <?php echo json_encode($logged_in_userid); ?> ;
 </script>
 <div class="header">
     <div class="container">
