2d0dbf9b64dd30ba2a46ae17f7b77ee3591dac8d
Alex Bates <16batesa@gmail.com>
2015-04-15 19:31:49 +0100

    Update connect.php

--- Site/assets/includes/connect.php
+++ Site/assets/includes/connect.php
@@ -10,9 +10,11 @@
         $logged_in_userid = $_SESSION["userId"];
         $html = file_get_html('http://opensprites.gwiddle.co.uk/forums/?p=member/' . $logged_in_userid);
         $logged_in_user = $html->find('h1#memberName', 0)->innertext;
-        $user_banned = $html->find('p#memberGroup', 0)->innertext;
+        $user_group = $html->find('p#memberGroup', 0)->innertext;
+        $user_banned = $user_group == 'Suspended';
     } else {
         $logged_in_userid = 'not logged in';
         $logged_in_user = 'not logged in';
+        $user_group = 'not logged in';
         $user_banned = false;
     }
