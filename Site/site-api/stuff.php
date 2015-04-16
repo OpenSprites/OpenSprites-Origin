20d779b44f18712403b8021aabf07b9c2684886d
Alex Bates <16batesa@gmail.com>
2015-04-16 16:28:14 +0100

    Create stuff.php

---
+++ Site/site-api/stuff.php
@@ -0,0 +1,26 @@
+<?php
+require '../assets/includes/html_dom_parser.php';
+session_name("OpenSprites_Forum_session");
+session_start();
+header('Access-Control-Allow-Origin: *');
+header("Access-Control-Allow-Headers: Origin, x-requested-with, content-type, accept");
+header('Cache-Control: no-cache, must-revalidate');
+header('Expires: Mon, 26 Jul 1997 05:00:00 GMT');
+header('Content-Type: application/json');
+
+$userid = 'false';
+if(isset($_GET['userid'])) {
+    $userid = $_GET['userid'];
+} else {
+    $userid = $_SESSION["userId"];
+}
+error_reporting(0);
+if($userid === 'false') {
+    echo 'FALSE';
+} else {
+    $files = glob('../uploads/uploaded/' . $logged_in_userid . '-*.*', GLOB_NOSORT);
+    for ($i = 1; $i <= count($files); $i++) {
+        echo $files[i];
+    }
+}
+?>
