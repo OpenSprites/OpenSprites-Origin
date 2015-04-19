f94512519d43a6dcf78ecc09ad8dec50d12fecc8
QuillzToxic <quillztoxic@gmail.com>
2015-04-19 21:45:19 +0100

    My first commit!! Blog Navbar updates.

--- Blog/includes.php
+++ Blog/includes.php
@@ -1,7 +1,7 @@
 <?php
 
 
-    include("sidebar.php"); // Eventually I'll add this in. :P
+    include("navbar.php"); // Eventually I'll add this in. :P
     include("footer.php");  // Some nice CSS styling should make this appear where it should go.
 
 
