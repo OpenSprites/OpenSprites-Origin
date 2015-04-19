1e5d3845b20930ec55d52b45787914ed7b4b4054
Alex Bates <16batesa@gmail.com>
2015-04-19 16:42:11 +0100

    Update breakout.js

--- Site/assets/js/breakout.js
+++ Site/assets/js/breakout.js
@@ -1,5 +1,5 @@
-//detects if your page is trapped inside someone elses frames and automatically breaks out
-if(top != self) {
- top.onbeforeunload = function() {};
- top.location.replace(self.location.href);
+// detects if your page is trapped inside someone elses frames and automatically breaks out
+if(top != self && top.location.host !== "dev.opensprites.gwiddle.co.uk") {
+    top.onbeforeunload = function() {};
+    top.location.replace(self.location.href);
 }
