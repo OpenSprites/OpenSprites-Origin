f0d326f771557cebacfa1582513311c5f343155a
QuillzToxic <QuillzToxic@gmail.com>
2015-04-20 08:04:03 +0100

    Updating Blog

---
+++ Blog/navbar.php
@@ -0,0 +1,24 @@
+<div class="header">
+    <div class="container">
+        <a class="scratch" href="http://dev.opensprites.gwiddle.co.uk/"></a>
+
+        <ul class="left">
+            <li>
+                <a href="http://dev.opensprites.gwiddle.co.uk/sprites">Sprites</a>
+            </li>
+            <li>
+                <a href="http://dev.opensprites.gwiddle.co.uk/scripts">Scripts</a>
+            </li>
+            <li>
+                <a href="http://dev.opensprites.gwiddle.co.uk/media">Media</a>
+            </li>
+            <li>
+                <a href="index.php">Blog</a>
+            </li>
+            <li>
+                <a href="blogoverview.php">Archives</a>
+            </li>
+            <li class="last">
+                <a href="http://opensprites.gwiddle.co.uk/forums/">Forums</a>
+            </li>
+        </ul>
