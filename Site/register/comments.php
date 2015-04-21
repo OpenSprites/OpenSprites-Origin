1cd12e6aceabfad3569fa8aaf4295a2cc2cbd722
Alex Bates <16batesa@gmail.com>
2015-04-21 20:15:16 +0100

    Update comments.php

--- Site/register/comments.php
+++ Site/register/comments.php
@@ -5,8 +5,8 @@ $project_comments = file_get_html('http://scratch.mit.edu/site-api/comments/proj
 $comments = $project_comments -> find('.comment .info');
 $is_good_reg = false;
 foreach ($comments as $comment) {
-	$creator = $comment -> find('.name', 0) -> plaintext;
-	$content = $comment -> find('.content', 0) -> plaintext;
+	$creator = trim($comment -> find('.name', 0) -> plaintext);
+	$content = trim($comment -> find('.content', 0) -> plaintext);
 	echo $creator . ' : ' . $logged_in_user . '<br>';
 	echo $content . '<br>&nbsp;<br>';
 	if ($creator == $logged_in_user && $content == $_GET['key']) {
