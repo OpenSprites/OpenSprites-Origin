1fa1d0714d137cecb51dc8d090db3ce50d2e36fe
MegaApuTurkUltra <apu@aputurk.tk>
2015-04-21 18:46:49 -0400

    Modified models.js

--- Site/assets/js/models.js
+++ Site/assets/js/models.js
@@ -5,14 +5,7 @@ OpenSprites.models.AssetList = function(json){
 	for(var i = 0;i<json.length;i++) {
 		var html = $("<div>").addClass("file").addClass(json[i].type).attr("data-name", json[i].name).attr("data-utime", json[i].upload_time);
 		if(json[i].type == "image"){ 
-			html.css("background-image", "url(/assets/images/loading-circle.gif)");
-			(function(url, element){
-				$("<img>").load(function(){
-					element.css("background-image", "url("+url+")");
-				}).error(function(){
-					element.css("background-image", "url(/assets/images/defaultimage.png)");
-				}).attr("src", url);
-			})(OpenSprites.domain + json[i].url, html);
+			html.css("background-image", "url("+OpenSprites.domain + json[i].url+")");
 		}
         model.append(html);
     }
