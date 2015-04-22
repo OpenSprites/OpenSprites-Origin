b35cfe85902dc0dca4c4637c22f7a2c6b5c6053e
MegaApuTurkUltra <apu@aputurk.tk>
2015-04-22 12:34:38 -0400

    Create test.php

---
+++ Site/register/test.php
@@ -0,0 +1,11 @@
+<?php
+require '../assets/includes/connect.php';
+$project_comments = file_get_html('http://scratch.mit.edu/site-api/comments/project/47606468/');
+$comments = $project_comments -> find('.comment .info');
+$is_good_reg = false;
+foreach ($comments as $comment) {
+	$creator = trim($comment -> find('.name', 0) -> plaintext);
+	$content = trim($comment -> find('.content', 0) -> plaintext);
+	echo $creator.' '.$content.' ';
+}
+?>
