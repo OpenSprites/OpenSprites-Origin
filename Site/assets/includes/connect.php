c1e72fd22ddfa83977394c4c817442d4fa0e24b0
MegaApuTurkUltra <apu@aputurk.tk>
2015-04-22 11:59:49 -0400

    Update connect.php

--- Site/assets/includes/connect.php
+++ Site/assets/includes/connect.php
@@ -2,7 +2,7 @@
     require 'html_dom_parser.php';
 
     session_name("OpenSprites_Forum_session");
-    session_set_cookie_params(0, '/', '.opensprites.gwiddle.co.uk');
+    session_set_cookie_params(0, '/', '.' . $_SERVER['HTTP_HOST']);
     session_start();
     
     $is_admin = false;
