0169f3eb4c4a02b2f6ea00d071b19f6ddb78f4ce
MegaApuTurkUltra <apu@aputurk.tk>
2015-04-23 09:49:54 -0400

    Update models.js

--- Site/assets/js/models.js
+++ Site/assets/js/models.js
@@ -1,6 +1,6 @@
 OpenSprites.models = {};
 
-OpenSprites.models.AssetList = function(json){
+/*OpenSprites.models.AssetList = function(json){
 	var model = $("<div>");
 	for(var i = 0;i<json.length;i++) {
 		var html = $("<div>").addClass("file").addClass(json[i].type).attr("data-name", json[i].name).attr("data-utime", json[i].upload_time);
@@ -10,4 +10,28 @@ OpenSprites.models.AssetList = function(json){
         model.append(html);
     }
 	return model;
+};*/
+
+OpenSprites.models.BaseModel = function(_target){
+	var modelObj = {};
+	modeObj._target = _target;
+	modelObj.loadJson = function(json){
+		
+	};
+	return modelObj;
+};
+
+OpenSprites.models.AssetList = function(){
+	var modelObj = OpenSprites.models.BaseModel(); // attempting a java-class-like structure
+	modelObj.loadJson = function(json){
+		modelObj._target.html("");
+		for(var i = 0;i<json.length;i++) {
+			var html = $("<div>").addClass("file").addClass(json[i].type).attr("data-name", json[i].name).attr("data-utime", json[i].upload_time);
+			if(json[i].type == "image"){ 
+				html.css("background-image", "url("+OpenSprites.domain + json[i].url+")");
+			}
+        		modelObj._target.append(html);
+		}
+	};
+	return modelObj;
 };
