1a3691653c417e6f2dcb8a74374f11d6b9cb4154
Alex Bates <16batesa@gmail.com>
2015-04-24 17:04:48 +0100

    Update file.php

--- Site/users/file.php
+++ Site/users/file.php
@@ -2,7 +2,7 @@
     require "../assets/includes/connect.php";  //Connect - includes session_start();
     $file = json_decode(file_get_contents('../site-api/asset.php?userid=' . $_GET['id'] . '&hash=' . $_GET['file']));
     if(count($file) == 0) {
-        header('Location: /');
+        //header('Location: /');
     }
 ?>
 <!DOCTYPE html>
