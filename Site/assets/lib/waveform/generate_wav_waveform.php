8f76dd66f4eef73002a4886fa5ae32371e43f591
MegaApuTurkUltra <apu@aputurk.tk>
2015-04-24 15:59:18 -0400

    Fix waveforms

--- Site/assets/lib/waveform/generate_wav_waveform.php
+++ Site/assets/lib/waveform/generate_wav_waveform.php
@@ -1,4 +1,16 @@
 <?php
+	include "Waveform.php";
+	include "Wave.php";
+	include "Exception.php";
+	include "Channel.php";
+	include "Generator/Png.php";
+	include "Generator/GeneratorInterface.php";
+	include "Chunk/ChunkAbstract.php";
+	include "Chunk/ChunkInterface.php";
+	include "Chunk/Data.php";
+	include "Chunk/Fmt.php";
+	include "Chunk/Other.php";
+
 	use BoyHagemann\Waveform\Waveform;
 	use BoyHagemann\Waveform\Generator;
 	$waveform =  Waveform::fromFilename($file);
