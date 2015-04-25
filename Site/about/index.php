4535783d2e33c9daaeca07d6dc9b8c2eefb612b5
MegaApuTurkUltra <apu@aputurk.tk>
2015-04-25 13:23:25 -0400

    Update about

--- Site/about/index.php
+++ Site/about/index.php
@@ -20,11 +20,13 @@
 	<div class="container main">
 		<div class="main-inner">
 		<h2 id="about-title">About us</h2>
-            <p>OpenSprites is a free, open-source site that allows <a href='https://scratch.mit.edu'>Scratch</a> users to share their own scripts, sprites and project media. The site is almost entirley open-source, meaning that you can examine, modify and share our code without running into trouble. If you'd like to take a look at our souce-code, head over to our GitHub <a href="https://github.com/OpenSprites">organisation</a>.</p>
+            <p>OpenSprites is a free, open-source site made by the <a href="https://github.com/OpenSprites/OpenSprites/blob/master/README.md" target="_blank">OpenSprites team</a> that allows <a href='https://scratch.mit.edu'>Scratch</a> users to share their own scripts, sprites and project media. The site is almost entirely open-source, meaning that you can examine, modify and share our code without running into trouble. If you'd like to take a look at our source code, head over to our <a href="https://github.com/OpenSprites">GitHub organization</a>.</p>
+		
+		<!-- Move statistics to its own page
 		<p>Statistics:</p>
-		<p>Users on OpenSprites: <?php echo "32"; ?></p>
+		<p>Users on OpenSprites: <?php /* lol u serious? */ echo "32"; ?></p>
 		<p>Total Sprites shared: <?php echo "321"; ?></p>
-		<p>Total Scripts shared: <?php echo "193"; ?></p>
+		<p>Total Scripts shared: <?php echo "193"; ?></p> -->
     </div>
 	</div>
 
