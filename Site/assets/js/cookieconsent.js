c7c2a6b7cb4b27bbf1d0ab2e9a73689ead9ef0a9
cheeseeater <cheese.eater@mail.com>
2015-04-25 07:45:54 +0100

    Cookie banner

--- Site/assets/js/cookieconsent.js
+++ Site/assets/js/cookieconsent.js
@@ -2,7 +2,7 @@
     $(function () {
         if(localStorage['cookies'] === undefined) {
             // Europe Cookie Consent Law - shows for all visitors as GeoBytes sometimes doesn't work
-            $('head').append('<style>#cookielaw-text {position:absolute;left:40px;font-size:21px;height:27px;line-height:24px;} #cookielaw {position:fixed;bottom:0;right:0;width:100%;font-size:18px;line-height:18px;padding:12px;background:rgb(174,207,54);color:black;}</style>');
+            $('head').append('<style>#cookielaw-text {position:absolute;left:40px;font-size:21px;height:27px;line-height:24px;z-index:99999;} #cookielaw {position:fixed;bottom:0;right:0;width:100%;font-size:18px;line-height:18px;padding:12px;background:rgb(174,207,54);color:black;}</style>');
             
             $('body').prepend('<div id="cookielaw"><div id="cookielaw-text">We use cookies on this site. Our <a href="http://dev.opensprites.gwiddle.co.uk/privacy/">Privacy Policy</a> outlines how we use cookies and what they are.</div><a href="javascript:$(\'#cookielaw\').fadeOut(300);localStorage[\'cookies\'] = true;" class="btn blue" style="float: right; width: 100px; text-align:center;">Close</a></div>');
         }
