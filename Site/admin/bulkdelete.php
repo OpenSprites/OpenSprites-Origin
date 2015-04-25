f1717ecb1df60a52849247aede908966b07559e1
Alex Bates <16batesa@gmail.com>
2015-04-25 10:25:16 +0100

    Create bulkdelete.php

---
+++ Site/admin/bulkdelete.php
@@ -0,0 +1,30 @@
+<?php
+require "../assets/includes/connect.php";
+
+if(!$is_admin) {
+    // user is not admin, display 404
+    include '../404.php';
+    die();
+}
+
+// bulk delete
+$json = json_decode(file_get_contents('../site-api/stuff.php?userid=' . $_GET['id']), true);
+?>
+
+<head>
+    <title>
+        OpenSprites Admin | Bulk Delete
+    </title>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script>
+    <link href="http://fonts.googleapis.com/css?family=Open+Sans:300,400" rel="stylesheet" type="text/css">
+    <link href="style.css" rel="stylesheet" type="text/css">
+</head>
+<body>
+    <div id='container'>
+        <h1>Admin | Bulk Delete</h1>
+        Click to delete a file- forever. Be careful!<br>
+        <?php
+        print_r($json);
+        ?>
+    </div>
+</body>
