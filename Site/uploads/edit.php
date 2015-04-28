69126d723d6cb742ba962dd997bf4a80eceae983
MegaApuTurkUltra <apu@aputurk.tk>
2015-04-28 16:18:02 -0400

    Update edit backend

--- Site/uploads/edit.php
+++ Site/uploads/edit.php
@@ -4,10 +4,40 @@ include '../assets/includes/connect.php';
 
 header("Content-Type: application/json");
 
-if(!isset($_GET['hash'])) {
-	die(json_encode(array("status" => "error", "message" => "missing params")));
+if(!isset($_GET['hash']) || $logged_in_userid == 0) {
+	die(json_encode(array("status" => "error", "message" => "Missing params")));
 }
 $hash = $_GET['hash'];
 
+$query = "UPDATE `" . getAssetsTableName() . "` SET";
+$params = array();
+if(isset($_GET['title'])){
+	$query .= " `customName`=?";
+	$title = $_GET['title'];
+	if(strlen($title) > 32) $title = substr($title, 0, 32);
+	array_push($params, $title);
+}
 
+if(isset($_GET['description'])){
+	if(isset($_GET['title'])){
+		$query .= ",";
+	}
+	$query .= " `description`=?";
+	$desc = $_GET['description'];
+	if(strlen($desc) > 500) $desc = substr($desc, 0, 500);
+	array_push($params, $desc);
+}
+
+$query .= " WHERE `userid`=? AND `hash`=?";
+
+array_push($params, $logged_in_userid, $hash);
+
+try {
+	connectDatabase();
+	imagesQuery0($query, $params);
+} catch(Exception $e){
+	die(json_encode(array("status" => "error", "message" => "Database error")));
+}
+
+echo json_encode(array("status" => "success", "message" => "Updated"));
 ?>
