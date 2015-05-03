e729c30d958ebc9f5e84fdc610295f3be14703fc
Andrew <andrewjcolescratch@gmail.com>
2015-05-03 09:52:52 -0700

    Fixed for longer usernames

--- Site/app/username.php
+++ Site/app/username.php
@@ -4,12 +4,21 @@
 <!DOCTYPE html>
 <html>
 <head>
-  <title>Logged in user</title>
-  <style>
-  @import url(http://fonts.googleapis.com/css?family=Open+Sans:300);
-  </style>
+    <title>Logged in user</title>
+    <style>
+    @import url(http://fonts.googleapis.com/css?family=Open+Sans:300);
+  
+    span {
+	margin:0px;
+    display:inline-block;
+    width:130px;
+    white-space: nowrap;
+    overflow:hidden !important;
+    text-overflow: ellipsis;
+    }
+    </style>
 </head>
-<body style="background-color:#659593;font-family:Open Sans;color:white">
-  <?php echo $logged_in_user; ?>
+<body style="background-color:#659593;font-family:Open Sans;color:white;margin:0">
+    <span><?php echo $logged_in_user; ?></span>
 </body>
 </html>
