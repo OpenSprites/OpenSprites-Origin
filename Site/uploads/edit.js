21f72cfee99dc2f83a1557ba2b9f521527f03199
Alex Bates <16batesa@gmail.com>
2015-05-06 16:56:37 +0100

    Update edit.js

--- Site/uploads/edit.js
+++ Site/uploads/edit.js
@@ -2,56 +2,56 @@ $(".file_rename").click(function(){
 	$("#file-name").val(OpenSprites.view.file.name);
 	$("#file-desc").val(OpenSprites.view.file.description);
 	$("#file-rename-dialog .input-error").text("Sample Text").css("opacity", "0");
-	$(".modal-bg, #file-rename-dialog").fadeIn();
+	$(".modal-overlay, .modal").fadeIn();
 });
 
 $("#file-name").keyup(function(){
 	if($(this).val().length > 32){
-		$("#file-rename-dialog .input-error").text("You must enter a name for your resource less than 32 characters long").css("opacity", "1");
+		$(".modal .input-error").text("You must enter a name for your resource less than 32 characters long").css("opacity", "1");
 	} else {
-		$("#file-rename-dialog .input-error").text("Sample Text").css("opacity", "0");
+		$(".modal .input-error").text("Sample Text").css("opacity", "0");
 	}
 });
 
 $("#file-desc").keyup(function(){
 	if($(this).val().length > 500){
-		$("#file-rename-dialog .input-error").text("You must enter a description for your resource less than 500 characters long").css("opacity", "1");
+		$(".modal .input-error").text("You must enter a description for your resource less than 500 characters long").css("opacity", "1");
 	} else {
-		$("#file-rename-dialog .input-error").text("Sample Text").css("opacity", "0");
+		$(".modal .input-error").text("Sample Text").css("opacity", "0");
 	}
 });
 
 
-$("#file-rename-dialog .cancel").click(function(){
-	$(".modal-bg, #file-rename-dialog").fadeOut();
+$(".modal .cancel").click(function(){
+	$(".modal-overlay, .modal").fadeOut();
 });
 
-$("#file-rename-dialog .ok").click(function(){
-	$("#file-rename-dialog .input-error").text("Sample Text").css("opacity", "0");
+$(".modal .ok").click(function(){
+	$(".modal .input-error").text("Sample Text").css("opacity", "0");
 	var name = $("#file-name").val();
 	var desc = $("#file-desc").val();
 	if(name == null || name == "" || name.length > 32){
-		$("#file-rename-dialog .input-error").text("You must enter a name for your resource less than 32 characters long").css("opacity", "1");
+		$(".modal .input-error").text("You must enter a name for your resource less than 32 characters long").css("opacity", "1");
 		return;
 	}
 	
 	if(desc == null || desc == "" || desc.length > 500){
-		$("#file-rename-dialog .input-error").text("You must enter a description for your resource less than 500 characters long").css("opacity", "1");
+		$(".modal .input-error").text("You must enter a description for your resource less than 500 characters long").css("opacity", "1");
 		return;
 	}
 	
-	$("#file-rename-dialog .rename-status").text("Saving...").parent().fadeIn();
+	$(".modal .rename-status").text("Saving...").parent().fadeIn();
 	
 	$.get("/uploads/edit.php", {hash: OpenSprites.view.file.md5, title: name, description: desc, userid: OpenSprites.view.file.uploaded_by.id}, function(data){
 		// userid is ignored unless you're an admin, in which case we need to know it
-		$("#file-rename-dialog .dialog-overlay").fadeOut();
+		$(".modal .dialog-overlay").fadeOut();
 		if(typeof data != "object"){
-			$("#file-rename-dialog .input-error").text("Whoops! Our servers sent back a bad response. Try again later.").css("opacity", "1");
+			$(".modal .input-error").text("Whoops! Our servers sent back a bad response. Try again later.").css("opacity", "1");
 			return;
 		}
 		
 		if(data.status != "success"){
-			$("#file-rename-dialog .input-error").text(data.message).css("opacity", "1");
+			$(".modal .input-error").text(data.message).css("opacity", "1");
 			return;
 		}
 		
@@ -60,10 +60,10 @@ $("#file-rename-dialog .ok").click(function(){
 		OpenSprites.view.file.description = data.description;
 		$(".desc").html(data.description);
 		
-		$(".modal-bg, #file-rename-dialog").fadeOut();
+		$(".modal-overlay, .modal").fadeOut();
 	}).fail(function(){
-		$("#file-rename-dialog .dialog-overlay").fadeOut();
-		$("#file-rename-dialog .input-error").text("Whoops! A problem prevented us from receiving a response from our servers.").css("opacity", "1");
+		$(".modal .dialog-overlay").fadeOut();
+		$(".modal .input-error").text("Whoops! A problem prevented us from receiving a response from our servers.").css("opacity", "1");
 	});
 });
 
