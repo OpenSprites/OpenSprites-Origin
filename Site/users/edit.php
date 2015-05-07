f2a23cfc824aaa5e4e3d32b0385526f58efd5e65
Alex Bates <16batesa@gmail.com>
2015-05-07 20:18:24 +0100

    Update edit.php

--- Site/users/edit.php
+++ Site/users/edit.php
@@ -1,13 +1,12 @@
 <?php
 include '../assets/includes/connect.php';
 
-if(!isset($_GET['json']) || ($logged_in_userid == 0)) {
-	die("Missing params");
+if($logged_in_userid == 0 || $logged_in_userid == 'not_logged_in') {
+	header("Location: /");
 }
 
-$json = json_decode($_GET['json']);
-
 connectForumDatabase();
-echo json_encode(getProfileSettings($logged_in_userid), JSON_PRETTY_PRINT);
+setProfileSettings($logged_in_userid, $_GET['bgcolor']);
 
+header("Location: /users/$logged_in_userid/");
 ?>
