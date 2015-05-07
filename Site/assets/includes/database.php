afce81e1098177aeaf24585c037958761ac59716
Alex Bates <16batesa@gmail.com>
2015-05-07 20:41:11 +0100

    Update database.php

--- Site/assets/includes/database.php
+++ Site/assets/includes/database.php
@@ -73,13 +73,9 @@ function getProfileSettings($userid){
 	global $forum_dbh;
 	
 	$res = forumQuery("SELECT * FROM `os_profile_settings` WHERE `userid`=?", array($userid));
-	if(sizeof($res) == 0) return array("bgcolor" => "avatar");
+	if(sizeof($res) == 0) return false;
 	
-<<<<<<< HEAD
-	return $res;
-=======
-	return array('bgcolor' => $res[0]['bgcolor']);
->>>>>>> d21bfd6d451e90a674b967e1e8dc0a8cfb796674
+	return $res[0];
 }
 
 function getUserInfo($userid){
