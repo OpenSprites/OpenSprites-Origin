3dad534c12e297c9e63cdea35b4e0ec94be6a27f
Alex Bates <16batesa@gmail.com>
2015-05-08 16:37:37 +0100

    Update navbar.php

--- public_html/blog/navbar.php
+++ public_html/blog/navbar.php
@@ -1,31 +1,5 @@
 <div class="header">
     <div class="container">
         <a class="scratch" href="http://opensprites.gwiddle.co.uk"></a>
-
-        <ul class="left">
-            <li>
-                <a href="http://opensprites.gwiddle.co.uk/sprites">Sprites</a>
-            </li>
-            <li>
-                <a href="http://opensprites.gwiddle.co.uk/scripts">Scripts</a>
-            </li>
-            <li>
-                <a href="http://opensprites.gwiddle.co.uk/media">Media</a>
-            </li>
-            <li>
-                <a href="http://blog.opensprites.gwiddle.co.uk">Blog</a>
-            </li>
-            <li class="last">
-                <a href="http://opensprites.gwiddle.co.uk/forums/">Forums</a>
-            </li>
-        </ul>
-
-        <!--
-        <ul class="right">
-            <li class="last" onclick="window.location = '';">
-                Archive
-            </li>
-        </ul>
-        -->
     </div>
 </div>
