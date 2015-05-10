5ffe8b34d50f9d17465bf1ee6a572ef6c4b43202
MegaApuTurkUltra <apu@aputurk.tk>
2015-05-10 16:32:05 -0400

    Set connect vars properly

--- public_html/assets/includes/connect.php
+++ public_html/assets/includes/connect.php
@@ -37,36 +37,19 @@
 	connectDatabase();
     
     $is_admin = false;
+	$logged_in_userid = 0;
+    $user = 'not logged in';
+    $logged_in_user = 'not logged in';
+    $user_group = 'N/A';
+    $user_banned = false;
     if(isset($_SESSION["userId"])) {
-        /*$user = json_decode(file_get_contents('http://opensprites.gwiddle.co.uk/site-api/user.php'));
-        
-        $logged_in_userid = $_SESSION["userId"];
-        $html = file_get_html('http://opensprites.gwiddle.co.uk/forums/?p=member/' . $logged_in_userid);
-        $logged_in_user = $html->find('h1#memberName', 0)->innertext;
-        $user_group = $html->find('p#memberGroup', 0)->plaintext;
-        $user_banned = $user_group == 'Suspended';
-
-        if($user_group == "Moderator" or $user_group == "Administrator") {
-            $is_admin = true;
-        }
-
-        if($user_group == "Suspended"){
-            header( 'Location: http://opensprites.gwiddle.co.uk/suspended.php' ) ;
-        }*/
-		
 		$userInfo = getUserInfo(intval($_SESSION["userId"]));
 		$logged_in_userid = $userInfo['userid'];
 		$logged_in_user = $userInfo['username'];
 		$user_group = ucwords($userInfo['usertype']);
-		$user_banned = $user_group == 'Suspended';
+		$user_banned = ($user_group == 'suspended');
 		$is_admin = $user_group == 'administrator' || in_array("Moderator", $userInfo['groups']);
 		if(!$is_admin){
 			error_reporting(0);
 		}
-    } else {
-        $logged_in_userid = 0;
-        $user = 'not logged in';
-        $logged_in_user = 'not logged in';
-        $user_group = 'N/A';
-        $user_banned = false;
     }
