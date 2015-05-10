fdf2af5c6bd7f035cde8f66478cb1c594f88d0af
Liam (Aqua) <liam4@users.noreply.github.com>
2015-05-10 20:38:58 -0300

    Fix the terrible tabbing in suspended.php
    Yeah. Also fix a typo clas to class in <div clas="container main">.

--- public_html/suspended.php
+++ public_html/suspended.php
@@ -7,17 +7,17 @@
   </head>
   <body>
   <?php include 'navbar.php'; ?>
-      <div clas="container main">
-                  <div class="main-inner">
-                <div class="box" style="width:50%;margin-left:auto;margin-right:auto">
-                    <h1 id="opensprites-heading">Account Suspended</h1>
-                    <div class="box-content" align="center">
-                        <img src="img/fly-you-fools.gif" />
-                      <p>Please contact us to appeal your suspension, or if you think this is in error.</p>
-                  </div>
-                    </div>
-          </div>
+  <div class="container main">
+    <div class="main-inner">
+      <div class="box" style="width:50%;margin-left:auto;margin-right:auto">
+        <h1 id="opensprites-heading">Account Suspended</h1>
+        <div class="box-content" align="center">
+          <img src="img/fly-you-fools.gif" />
+          <p>Please contact us to appeal your suspension, or if you think this is in error.</p>
+        </div>
       </div>
+    </div>
+  </div>
   <?php include 'footer.html'; ?>
   </body>
 </html>
