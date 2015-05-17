c80ace661131902e937a73db131f8b489c4b85b9
cheeseeater <The-Cheese-Eater@users.noreply.github.com>
2015-05-17 07:46:28 +0100

    Use navbar and footer from main site

--- public_html/blog/includes.php
+++ public_html/blog/includes.php
@@ -1,8 +1,8 @@
 <?php
 
     restore_include_path();
-    include("navbar.php"); // Eventually I'll add this in. :P
-    include("footer.php");  // Some nice CSS styling should make this appear where it should go.
+    include("//navbar.php");
+    include("//footer.php");
 
 
 ?>
