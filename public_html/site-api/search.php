d589b1dc6677ecb1e459d09ae0d39e699f424199
MegaApuTurkUltra <apu@aputurk.tk>
2015-05-17 15:25:46 -0400

    Made advanced searches possible

--- public_html/site-api/search.php
+++ public_html/site-api/search.php
@@ -7,17 +7,65 @@ function getNiceResultNumber($num){
 	return "About " . floor($num / 1000000) . "M results";
 }
 
-if(!isset($_GET['query'])) die(json_encode(array("message" => "Missing query", "results" => array())));
+if(!isset($_GET['query']) && !isset($_POST['advancedQuery'])) die(json_encode(array("message" => "Missing query", "results" => array())));
 
 $query = $_GET['query'];
-$words = preg_split("/[^a-zA-Z0-9]+/", $query, -1, PREG_SPLIT_NO_EMPTY);
+$words = preg_split("/[^a-zA-Z0-9\+\-\*\"]+/", $query, -1, PREG_SPLIT_NO_EMPTY);
 
-$sorts = ["popularity", "date", "alphabetical"];
+$warning = array();
+
+$keywords = array();
+
+function is_valid_word($word){
+	return preg_match("/^[a-zA-Z0-9]+$/", $word);
+}
+
+$size = sizeof($words);
+for($i=0;$i<$size;$i++){
+	$word = $words[$i];
+	if(substr($word, 0, 1) === "+" && is_valid_word(substr($word, 1, strlen($word) - 1))){
+		array_push($keywords, $word);
+	} else if(substr($word, 0, 1) === "-" && is_valid_word(substr($word, 1, strlen($word) - 1))){
+		array_push($keywords, $word);
+	} else if(substr($word, strlen($word) - 1, 1) === "*" && is_valid_word(substr($word, 0, strlen($word) - 1))){
+		array_push($keywords, $word);
+	} else if(substr($word, 0, 1) === "\"" && is_valid_word(substr($word, 1, strlen($word) - 1))){
+		$j = $i + 1;
+		$quote = $word;
+		while(substr($words[$j], strlen($words[$j]) - 1, 1) !== "\""){
+			if(is_valid_word($words[$j])){
+				$quote .= " " . $words[$j];
+			} else {
+				$warning = "Malformed query (not a valid word in quote)";
+			}
+			$j++;
+			if($j === $size - 1){
+				if(substr($words[$j], strlen($words[$j]) - 1, 1) !== "\""){
+					array_push($warning, "No closing quote on " . $words[$j]);
+				}
+				break;
+			}
+		}
+		if(is_valid_word(substr($words[$j], 0, strlen($words[$j]) - 1)) && substr($words[$j], strlen($words[$j]) - 1, 1) === "\""){
+			$quote .= " " . $words[$j];
+			array_push($keywords, $quote);
+		} else {
+			array_push($warning, $words[$j] . " is not a valid search word");
+		}
+		$i = $j;
+	} else if(is_valid_word($word)){
+		array_push($keywords, $word);
+	} else {
+		array_push($warning, $word . " is not a valid search word");
+	}
+}
+
+$sorts = ["relevance", "popularity", "date", "alphabetical"];
 $dirs = ["desc", "asc"];
 $places = ["both", "names", "descriptions"];
 $filters = ["all", "users", "resources", "collections"];
 
-$sort = "popularity";
+$sort = "relevance";
 $dir = "desc";
 $place = "both";
 $filter = "all";
@@ -28,7 +76,7 @@ if(isset($_GET['place'])) $place = $_GET['place'];
 if(isset($_GET['filter'])) $filter = $_GET['filter'];
 
 if(!in_array($sort, $sorts)){
-	$sort = "popularity";
+	$sort = "relevance";
 }
 if(!in_array($dir, $dirs)){
 	$dir = "desc";
@@ -43,10 +91,13 @@ if(!in_array($place, $places)){
 // a bug (?) in MySQL prevents using query parameters in MATCH, so we have to escape
 // http://stackoverflow.com/a/13682516/1021196
 
-$fulltext_search = join(" +", $words);
+$fulltext_search = join(" ", $keywords);
 $fulltext_search = getDbh()->quote($fulltext_search);
 
-$sql_query = "SELECT * FROM `os_assets` WHERE ";
+//                                                         \/ see above
+$match_query .= "MATCH(`customName`,`description`) AGAINST($fulltext_search IN BOOLEAN MODE) ";
+
+$sql_query = "SELECT *, $match_query as relevance FROM `os_assets` WHERE ";
 if($filter == "all"){
 	// add collections and users
 } else if($filter == "users"){
@@ -57,15 +108,18 @@ if($filter == "all"){
 	// welp, implement later
 }
 
-//                                                         \/ see above
-$sql_query .= "MATCH(`customName`,`description`) AGAINST($fulltext_search IN BOOLEAN MODE) ";
+$sql_query .= $match_query;
 
-if($sort == "popularity"){
+if($sort == "relevance"){
+	$sql_query .= " ORDER BY relevance ";
+} else if($sort == "popularity"){
 	$sql_query .= " ORDER BY `downloadsThisWeek` ";
 } else if($sort == "alphabetical"){
 	$sql_query .= " ORDER BY `customName` ";
 } else if($sort == "date"){
 	$sql_query .= " ORDER BY `date` ";
+} else {
+	$sql_query .= " ORDER BY relevance ";
 }
 
 if($dir == "desc"){
@@ -74,5 +128,5 @@ if($dir == "desc"){
 
 $res = imagesQuery($sql_query, array());
 
-echo json_encode(array("message" => getNiceResultNumber(sizeof($res)), "results" => getAssetList($res)));
+echo json_encode(array("message" => getNiceResultNumber(sizeof($res)), "warning" => $warning, "results" => getAssetList($res)));
 ?>
