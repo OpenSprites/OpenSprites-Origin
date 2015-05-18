f64bc580a40a8362215852e1d524b9916a3d720c
MegaApuTurkUltra <apu@aputurk.tk>
2015-05-18 16:36:52 -0400

    Style fixes

--- public_html/search/search.js
+++ public_html/search/search.js
@@ -1,7 +1,7 @@
 $(".fold.advanced-search .fold-toggle").click(function(){
 	var fold = $(".fold.advanced-search");
 	if(fold.hasClass("open")) fold.removeClass("open").addClass("closed");
-	else fold.removeClass("close").addClass("open");
+	else fold.removeClass("closed").addClass("open");
 });
 
 $("#search-input, #search-bar-input").val(OpenSprites.view.query);
