f2286fe4dc3e881918ac7d3e7395b692ec7edc71
Andrew <andrewjcolescratch@gmail.com>
2015-05-18 13:23:58 -0300

    Update index.php

--- public_html/mobile/userpanel/index.php
+++ public_html/mobile/userpanel/index.php
@@ -1,1 +1,17 @@
+<?php
+    require "../../assets/includes/connect.php";  //Connect - includes session_start();
+?>
+<!DOCTYPE html>
+<html>
+<head>
+    <!--Imports the metadata and information that will go in the <head> of every page-->
+    <?php include 'Header.html'; ?>
+</head>
+<body>
+<!--Imports navigation bar-->
+<?php include "navbar.php"; ?>
+
 EXISTS
+
+</body>
+</html>
