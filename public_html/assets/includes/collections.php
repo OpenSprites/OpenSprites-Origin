987569bcfe1060e05ac096e94c1ae404f7807213
MegaApuTurkUltra <apu@aputurk.tk>
2015-06-27 18:11:28 -0400

    Whoops, even more sneaky syntax errors

--- public_html/assets/includes/collections.php
+++ public_html/assets/includes/collections.php
@@ -5,7 +5,7 @@ function addCollection($user, $userId, $customName, $assets){
 	$res;
 	do {
 		$id = md5(uniqid(mt_rand()));
-		$res = imagesQuery("SELECT * FROM `" .getCollectionTableName() . "` WHERE `id`=?", array($id));
+		$res = imagesQuery("SELECT * FROM `" . getCollectionTableName() . "` WHERE `id`=?", array($id));
 	} while(sizeof($res) > 0);
 	
 	imagesQuery0("INSERT INTO `" . getCollectionTableName() . "` (`id`, `user`,`userid`,`customName`,`date`)"
@@ -18,21 +18,21 @@ function addCollection($user, $userId, $customName, $assets){
 }
 
 function addToCollection($userId, $collectionId, $asset){
-	imagesQuery0("INSERT INTO `" . getCollectionAssetTableName() "` (`userid`, `collectionid`, `assetuserid`, `assetid`)"
-			." VALUES(?, ?, ?, ?)", array($userId, $collectionId, $asset['userid'], $asset['hash']));
+	imagesQuery0("INSERT INTO `" . getCollectionAssetTableName() . "` (`userid`, `collectionid`, `assetuserid`, `assetid`)"
+			. " VALUES(?, ?, ?, ?)", array($userId, $collectionId, $asset['userid'], $asset['hash']));
 }
 
 function removeFromCollection($userId, $collectionId, $asset){
-	imagesQuery0("DELETE FROM `" . getCollectionAssetTableName() "` WHERE `userid`=? AND `collectionid`=? AND `assetuserid`=? AND `assetid`=?",
+	imagesQuery0("DELETE FROM `" . getCollectionAssetTableName() . "` WHERE `userid`=? AND `collectionid`=? AND `assetuserid`=? AND `assetid`=?",
 		array($userId, $collectionId, $asset['userid'], $asset['hash']));
 }
 
 function getCollectionInfo($userId, $collectionId){
-	return imagesQuery("SELECT * FROM `" . getCollectionTableName() "` WHERE `userid`=? AND `id`=?", array($userId, $collectionId));
+	return imagesQuery("SELECT * FROM `" . getCollectionTableName() . "` WHERE `userid`=? AND `id`=?", array($userId, $collectionId));
 }
 
 function getAssetsInCollection($userId, $collectionId){
-	return imagesQuery("SELECT * FROM `" . getCollectionAssetsTableName() "` WHERE `userid`=? AND `collectionid`=?", array($userId, $collectionId));
+	return imagesQuery("SELECT * FROM `" . getCollectionAssetsTableName() . "` WHERE `userid`=? AND `collectionid`=?", array($userId, $collectionId));
 }
 
 ?>
