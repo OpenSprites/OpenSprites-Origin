e0ac18de4d7839ef501d4e796d37fa52dc5c20a5
MegaApuTurkUltra <apu@aputurk.tk>
2015-07-02 12:15:35 -0400

    Finish iframe-less login

--- public_html/login/index-beta.php
+++ public_html/login/index-beta.php
@@ -13,6 +13,8 @@
 <head>
     <!--Imports the metadata and information that will go in the <head> of every page-->
     <?php include '../Header.html'; ?>
+	<script>
+		OpenSprites.view = <?php json_encode(array("return" => $return, "token" => $token)); ?>;
 </head>
 <body>
     <!--Imports site-wide main styling-->
@@ -24,27 +26,27 @@
     <!-- Main wrapper -->
     <div class="container main" style="height:440px;">
         <div class="main-inner">
-            <form action="//opensprites.org/forums/?p=user/login" method="post" enctype="multipart/form-data">
-				<input type="hidden" name="return" value="<?php echo $return; ?>">
+            <form enctype="multipart/form-data">
+				<div id="message"></div>
 				<input type="hidden" name="token" value="<?php echo $token; ?>">
 				<div class="sheetBody">
 					<div class="section">
 						<ul class="form">
 							<li><label>Username or Email</label>
 								<div class="fieldGroup">
-									<input type="text" name="username" value="" autofocus />
+									<input type="text" name="username" id="os-user" placeholder="Username or email" autofocus />
 								</div>
 							</li>
 							<li><label>Password <small><a href="/forums/?p=user/forgot" class="link-forgot" tabindex="-1">Forgot?</a></small></label>
 								<div class="fieldGroup">
-									<input type="password" name="password" value="">
+									<input type="password" name="password" id="os-pass" placeholder="Password" />
 								</div>
 							</li>
 						</ul>
 					</div>
 				</div>
 				<div class="buttons">
-					<input type="submit" name="login" value="Log In" class="big submit button" />
+					<input type="button" name="login" value="Log In" class="big submit button" />
 					<input type="button" name="cancel" value="Cancel" class="big cancel button" />
 				</div>
 			</form>
@@ -52,8 +54,39 @@
     </div>
 	
 	<script>
+		var allowed = true;
+		var waitSecs = 0;
+		var interval = 0;
+	
 		$(".button.cancel").click(function(){
-			location.href = <?php echo json_encode($return); ?>;
+			location.href = OpenSprites.view.return;
+		});
+		
+		$(".button.login").click(function(){
+			if(!allowed) return;
+			$.post("/site-api/login.php", {token: OpenSprites.view.token, username: $("#os-user").val(), password: $("#os-pass").val()}, function(data){
+				if(data.status == "success"){
+					location.href = OpenSprites.view.return;
+				} else {
+					$("#message").text(data.message);
+					if(data.hasOwnProperty(wait)){
+						allowed = false;
+						clearInterval(interval);
+						waitSecs = data.wait;
+						interval = setInterval(function(){
+							waitSecs--;
+							$("#message").text("Wrong username or password, wait " + waitSecs + " seconds before trying again");
+							if(waitSecs <= 0){
+								$("#message").text("Wrong username or password");
+								allowed = true;
+								clearInterval(interval);
+							}
+						}, 1000);
+					}
+				}
+			}).fail(function(){
+				$("#message").text("Whoops! We couldn't get a response from OpenSprites servers. Try again later.");
+			});
 		});
 	</script>
     
