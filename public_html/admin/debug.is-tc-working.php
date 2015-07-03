afd726d73c9ee9048529a00bcd90c262957cf5d0
MegaApuTurkUltra <apu@aputurk.tk>
2015-07-03 13:23:23 -0400

    Fix bug

--- public_html/admin/debug.is-tc-working.php
+++ public_html/admin/debug.is-tc-working.php
@@ -7,9 +7,11 @@ if(!$is_admin){
 
 $ht1 = file_get_contents("../.htaccess");
 $db1 = file_get_contents("../assets/includes/database.php");
+$ct1 = file_get_contents("../assets/includes/connect.php");
 
-$ht2 = file_get_contents("https://raw.githubusercontent.com/OpenSprites/OpenSprites/master/public_html/assets/includes/database.php");
-$db2 = file_get_contents("https://raw.githubusercontent.com/OpenSprites/OpenSprites/master/public_html/.htaccess");
+$ht2 = file_get_contents("https://raw.githubusercontent.com/OpenSprites/OpenSprites/master/public_html/.htaccess");
+$db2 = file_get_contents("https://raw.githubusercontent.com/OpenSprites/OpenSprites/master/public_html/assets/includes/database.php");
+$ct2 = file_get_contents("https://raw.githubusercontent.com/OpenSprites/OpenSprites/master/public_html/assets/includes/connect.php");
 
 echo ".htaccess: ";
 if($ht1 == $ht2){
@@ -19,10 +21,18 @@ if($ht1 == $ht2){
 }
 echo "\n";
 
-$db1 = preg_replace("/password = \"[^\"]+\"", "password = \"\"", $db1);
+$db1 = preg_replace("/password = \"[^\"]+\"/", "password = \"\"", $db1);
 
 echo "database.php: ";
 if($db1 == $db2){
+	echo "Up to date";
+} else {
+	echo "Not updated!";
+}
+echo "\n";
+
+echo "(test) connect.php: ";
+if($ct1 == $ct2){
 	echo "Up to date";
 } else {
 	echo "Not updated!";
