cca78aec4c6bf79f2d88060c4cfa07b4fc49ba15
MegaApuTurkUltra <apu@aputurk.tk>
2015-07-06 13:30:33 -0400

    Make error pages a bit nicer

--- public_html/404.php
+++ public_html/404.php
@@ -21,7 +21,14 @@
             <div id="about">
                 <img src='/assets/images/404.png' style='position: absolute; margin: auto; left: 0; right: 0;'>
                 <div style='width: 100%; height: 470px;'>&nbsp;</div>
-                <p style='position: absolute; margin: auto; top: 480px; left: 0; right: 0; width: 50%; text-align: center; font-size: 18px;'>We couldn't find the page you're looking for.<br>You may want to <a href='/'>go back to the main page</a>.</p>
+                <p style='position: absolute; margin: auto; top: 480px; left: 0; right: 0; width: 50%; text-align: center; font-size: 18px;'>We couldn't find the page you're looking for.<br>You may want to <a href='/'>go back to the main page</a>.<br/><br/>
+				</p>
+				<form action="/search.php" method="GET">
+					Search for: <input type="text" name="q" id="404-search" placeholder="Search" /><button type="submit">Search</button>
+				</form>
+				<script>
+					$("#404-search").val(location.pathname.replace(/[^a-zA-Z0-9]+/g, " ").trim());
+				</script>
             </div>
         </div>
     </div>
