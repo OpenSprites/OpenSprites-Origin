a100102c0a8f537ce42eb3a2ca33cb9474a0f67a
MegaApuTurkUltra <apu@aputurk.tk>
2015-07-05 20:27:19 -0400

    Fix tabindex again

--- public_html/login/index.php
+++ public_html/login/index.php
@@ -34,11 +34,11 @@
 				<div class="login-box">
 					<div class="sprite-login">Log In</div>
 					<div class="form-box">
-						<label for="username" class="sprite-username">Username <a class='small' href='/register' tabindex='3'>Create Account</a></label>
+						<label for="username" class="sprite-username">Username <a class='small' href='/register' tabindex='4'>Create Account</a></label>
 						<input type="text" class="login-field" id="username" tabindex='1' autofocus />
-						<label for="password" class="sprite-password">Password <a class='small' href='/forums/?p=user/forgot' tabindex='4'>Forgot Password?</a></label>
+						<label for="password" class="sprite-password">Password <a class='small' href='/forums/?p=user/forgot' tabindex='5'>Forgot Password?</a></label>
 						<input type="password" class="login-field" id="password" tabindex='2' />
-						<button type='submit' class="login-btn">Log in</button>
+						<button type='submit' class="login-btn" tabindex="3">Log in</button>
 					</div>
 				</div>
 				<br style="break:both" />
