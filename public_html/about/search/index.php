501a8d05b242841d19bf9ca5b3bb5adc6b4e637c
MegaApuTurkUltra <apu@aputurk.tk>
2015-07-06 14:02:05 -0400

    Compose search help page

--- public_html/about/search/index.php
+++ public_html/about/search/index.php
@@ -19,8 +19,60 @@
 	<!-- Main wrapper -->
 	<div class="container main">
 		<div class="main-inner">
-			Include some swag about search here.
+			<h2>Notice</h2>
+			<p>Search is incomplete as of right now. It cannot search for users or collections.</p>
+			<h2>Basic searches</h2>
+			<p>To do a basic search, enter the thing you want to search for, including only letters and numbers, then press the search button. If your search gives more than 20 results, the results will be categorized in pages. Press a button for a page number to switch pages.</p>
+			<h2>Basic search options</h2>
+			<p>The four basic search options are
+				<ul>
+					<li><b>Sort by:</b>
+						<ul>
+							<li>Relevance: This sorts the results and puts the ones that match your search the most on top</li>
+							<li>Popularity: This sorts results by number of downloads (for assets and collections).</li>
+							<li>Date: This sorts results by the date they were created</li>
+							<li>Alphabetical: This sorts results alphabetically</li>
+						</ul>
+					</li>
+					<li><b>Sort direction:</b>
+						<ul>
+							<li>Descending: The default direction for sort, ie most relevant, most popular, earliest date, A-Z</li>
+							<li>Ascending: Inverted sort direction, ie least relevant, least popular, latest date, Z-A</li>
+						</ul>
+					</li>
+					<li><b>Search in:</b>
+						<ul>
+							<li>Both: Searches in all areas of profiles, asset pages, and collection pages</li>
+							<li>Usernames/Titles: Searches only the usernames of users, and titles of assets and collections only</li>
+							<li>Descriptions/About Sections: Searches in descriptions of assets and collections, and in about sections of users only</li>
+						</ul>
+					</li>
+					<li><b>Filter by:</b>
+						<ul>
+							<li>All: Displays all results</li>
+							<li>Users: Displays only results that are users</li>
+							<li>Resources: Displays only results that are scripts, images, or sounds</li>
+							<li>Collections: Displays only results that are collections</li>
+						</ul>
+					</li>
+				</ul>
+			</p>
+			<h2>Advanced searching</h2>
+			<p>Expand the advanced search section to do an advanced search. You can also enter the symbols for advanced searches directly in the search bar. Multiple advanced search keywords can be used in the same search query.</p>
+			<p><sub><sup>Note: Some advanced users may note that the following system is exactly how <i>MySQL boolean searches</i> work.</sup></sub></p>
+			<p>The advanced search options are:
+				<ul>
+					<li><b>Some of these words</b> <i>(no symbol)</i>: Searches for results that contain one or more of the words, and the "Relevance" sort will sort results by the number of matched words. To use this in the search bar, enter space-separated words as usual. This is the default search mode.</li>
+					<li><b>All of these words</b> <i>+word</i>: Searches only for results that contain all of the given words. If a result does not contain one or more of the words, it is excluded. For example, <pre><code>+world</code></pre> would match "hello world" but not "hello opensprites"</li>
+					<li><b>None of these words</b> <i>-word</i>: Searches only for results that do not contain any of the given words. If a result contains one, it is excluded. For example, <pre><code>-hello</code></pre> would match "opensprites is cool" but not "hello opensprites"</li>
+					<li><b>Words starting with</b> <i>word*</i>: Searches for words starting with the given text. For example, <pre><code>open*</code></pre> would match both "open" and "opensprites"</li>
+					<li><b>This phrase</b> <i>"some phrase"</i>: Searches for results that contain the given phrase as it is written. For example, <pre><code>"apple banana"</code></pre> would match "apple banana canteloupe" but not "banana apple"</li>
+				</ul>
+			</p>
+			<h2>Need more help?</h2>
+			<p>You can post for help on the <a href="http://opensprites.org/forums/">forums</a> for a response from OpenSprites users, or send a ticket to our <a href="https://opensprites.atlassian.net/servicedesk/customer/portal/2" target="_blank">help desk</a> for a response from OpenSprites moderators.</p>
 		</div>
+		<br/><br/>
 	</div>
 	
 	<!-- footer -->
