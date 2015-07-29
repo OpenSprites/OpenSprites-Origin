5c1d1281414b59918fe6b401b808d0359b6896fe
Rumanti <emma.rumanti@gmail.com>
2015-07-30 06:51:29 +0700

    style things + more readable code

--- public_html/users/collections.php
+++ public_html/users/collections.php
@@ -40,10 +40,22 @@ if($is_creating){
 			font-weight:300;
 			text-align:center;
 		}
+        
 		#id {
 			font-size:20px;
 			margin-top:-10px;
 		}
+        
+        #description {
+            font-size:22px;
+            text-align:center;
+        }
+        
+        /* When creating collection */
+        #create-collection {
+            width:150px;
+            margin:auto;
+        }
 	</style>
 </head>
 <body>
@@ -57,20 +69,25 @@ if($is_creating){
 	<div class="container main">
 		<div class="main-inner">
 			<h1><?php echo ($is_creating ? "New Collection" : $info['customName'] . " <br><!--Is there a reason why we need this ID?--><div id='id'>(#" . $info['id'] . ")</div>"); ?></h1>
-			<div style="font-size:22px;text-align:center;">
+			<div id="description">
 				<strong>By: </strong> <a href="http://opensprites.org/users/<?php echo urlencode($username); ?>" target="blank"><?php echo ($uid === $logged_in_userid ? "You" : $username) /*. " (#" . $uid . ")"*/; ?></a>
 			</div>
+            
 			<!--<h2><?php //echo ($uid === $logged_in_userid ? "You" : $username) . " (#" . $uid . ")"; ?></h2>-->
+            
 			<?php if($is_creating){ ?>
 			<form id="create-collection">
 				<p class='status'></p>
 				<input type="text" name="name" id="collection-name" placeholder="Collection name" maxlength="32" /><br/>
-				<button type="submit">Create Collection</button>
+                <!-- I'll remove these deprecated <center> tags once I discovered a workaround.. -->
+				<center><button class="btn orange" type="submit">Create Collection</button></center>
 			</form>
+            
 			<?php } else { ?>
 			<div id="collection-description">
 				Render markdown here
 			</div><br/>
+            
 				<?php if($uid == $logged_in_userid){ ?>
 				<div id="collection-actions">
 					<p class='label' style='float:left;margin-top:2px;margin-right:8px;'>Actions:</p>
