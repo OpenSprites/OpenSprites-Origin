8f3bf7337a31f863ee9fe34cd0ae5e82e64683c7
Andrew <andrewjcolescratch@gmail.com>
2015-08-04 11:46:01 -0300

    Update test.php

--- public_html/app/test.php
+++ public_html/app/test.php
@@ -1,5 +1,5 @@
 <?php
-  $variable = file_get_contents('../site-api/user.php');
+  $variable = file_get_contents('http://opensprites.org/site-api/user.php');
   $decoded = json_decode($variable);
   echo $decoded;
 ?>
