15d06272c8660970dd18418efba5857e90d5ee63
DM5555 <DM5555>
2015-10-18 13:48:21 +0100

    Name-changing to reduce confusion
    @The-Cheese-Eater feel free to re-word this

--- public_html/index.php
+++ public_html/index.php
@@ -51,9 +51,9 @@
                 <p>Welcome to OpenSprites, the free open-source site that allows <a href='//scratch.mit.edu'>Scratch</a> users to share their own creations, such as scripts, sprites, sounds, and costumes! Designed with kids in mind, we promote the idea of creating and sharing, and provide a kid-safe platform for this. So, what are you waiting for? Get creating and sharing!</p>
             </div>
             
-			<div id="top-assets" class="box">
-                    <h1>Top Media</h1>
-                    <div class="box-content" id="top-assets-list"></div>
+			<div id="General Assets" class="box">
+                    <h1>General Media</h1>
+                    <div class="box-content" id="general-assets-list"></div>
             </div>
 
             <div id="feat-assets" class="box">
@@ -87,10 +87,7 @@
     <!-- footer -->
     <?php echo file_get_contents('footer.html'); ?>
 	<script>
-		var model = OpenSprites.models.SortableAssetList($("#top-assets-list"));
-		model = OpenSprites.models.SortableAssetList($("#feat-assets-list"));
-		model = OpenSprites.models.SortableAssetList($("#top-collections-list"));
-		model = OpenSprites.models.SortableAssetList($("#featured-collections"));
+		var model = OpenSprites.models.SortableAssetList($("#general-assets-list"));
 	</script>
 </body>
 </html>
