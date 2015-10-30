22815739e857ebc97db9a965306b54310b618de3
cheeseeater <cheese.eater@mail.com>
2015-10-30 19:12:08 +0000

    Easter egg test

--- public_html/index.php
+++ public_html/index.php
@@ -9,7 +9,7 @@
 </head>
 <body>
     <!-- Spider web easter egg for Halloween theme-->
-    <!--<img src="https://garretson.k12.sd.us/teacherresources/SiteAssets/SitePages/The%20Web!/white-spider-web-clipart-bcyE6k8Ki.gif" id="spiderweb" style="height:100px;width:100px;visibility:hidden;">-->
+    <img id="spider" src="https://garretson.k12.sd.us/teacherresources/SiteAssets/SitePages/The%20Web!/white-spider-web-clipart-bcyE6k8Ki.gif" id="spiderweb" style="height:100px;width:100px;visibility:hidden;">
     
     <!--Imports navigation bar-->
     <?php include "navbar.php"; ?>
