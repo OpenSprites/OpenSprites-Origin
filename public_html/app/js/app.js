e99de9b94716b90d85e93ad8c7b0cd0e490235c6
Alex Bates <16batesa@gmail.com>
2015-11-24 07:49:06 +0000

    Create app.js

---
+++ public_html/app/js/app.js
@@ -0,0 +1,11 @@
+// Initialize app
+var myApp = new Framework7();
+ 
+// If we need to use custom DOM library, let's save it to $$ variable:
+var $$ = Dom7;
+ 
+// Add view
+var mainView = myApp.addView('.view-main', {
+	// Because we want to use dynamic navbar, we need to enable it for this view:
+	dynamicNavbar: true
+});
