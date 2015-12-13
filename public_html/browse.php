f5539fe138165a32d309ba663c18f8a150e91b59
James <The-Cheese-Eater@users.noreply.github.com>
2015-12-13 12:16:00 +0000

    Titles formatting

--- public_html/browse.php
+++ public_html/browse.php
@@ -6,7 +6,7 @@
 	$allowed_types = array("media", "script", "collections");
 	
 	$descriptions = array(
-		"media" => "Find new and awesome resources from OpenSprites users here.",
+		"resources" => "Find new and awesome resources from OpenSprites users here.",
 		"scripts" => "Browse Scratch scripts from OpenSprites users, and download them for your own projects.",
 		"collections" => "Collections are groups resources. Some collections are meant to be used as sprites out of the box, others may just be a way of grouping different resources that have something in common. You can find new and awesome collections from OpenSprites users here!");
 	
@@ -46,21 +46,21 @@
 			
 			
             <div id="feat-assets">
-                    <h1>Featured <?php echo $displayType; ?></h1>
+                    <h1 class="centered-heading">Featured <?php echo $displayType; ?></h1>
                     <div class="box-content assets-list" id="feat-assets-list">
 						<p style="text-align: center;">Loading content, hang tight.<br/></p>
                     </div>
                 </div>
 			
             <div id="top-assets">
-                    <h1>Popular <?php echo $displayType; ?> From the Past Week</h1>
+                    <h1 class="centered-heading">Popular <?php echo $displayType; ?> From the Past Week</h1>
                     <div class="box-content assets-list" id="top-assets-list">
                         <p style="text-align: center;">Loading content, hang tight.<br/></p>
                     </div>
                 </div>
 			
 			<div id="new-assets">
-                    <h1>Newest <?php echo $displayType; ?></h1>
+                    <h1 class="centered-heading">Newest <?php echo $displayType; ?></h1>
                     <div class="box-content assets-list" id="new-assets-list">
                         <p style="text-align: center;">Loading content, hang tight.<br/></p>
                 </div>
