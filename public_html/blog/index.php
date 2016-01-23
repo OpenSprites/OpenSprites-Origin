f177e67ab8b51b324baed0cf18bd44b0f2285968
James <The-Cheese-Eater@users.noreply.github.com>
2016-01-23 12:23:55 +0000

    Restore working navbar

--- public_html/blog/index.php
+++ public_html/blog/index.php
@@ -3,6 +3,7 @@
     <head>
         <title>OpenSprites Blog</title>
         <link href='/blogmainstyle.css' type="text/css" rel=stylesheet>
+        <link href='../navbar.css' type="text/css" rel=stylesheet>
         <?php include("header.php"); ?>
     </head>
     <body>
