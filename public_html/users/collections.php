c8dc7ed83499500b138f11e4ec2798bfbc4a9a8c
Aaron <Arinerron@users.noreply.github.com>
2016-02-15 13:36:23 -0800

    Update collections.php

--- public_html/users/collections.php
+++ public_html/users/collections.php
@@ -105,6 +105,10 @@
                         <input type="text" placeholder="Search in collection" />
                     </div>
                     Render asset list here
+                    <div class="main container" id="collection-assets">
+                        <h2 class="centered-heading">Collection Resources</h2>
+                        <div class="box-content assets-list" id="collection-assets-list"></div>
+                    </div>
                 </div>
                 <?php } ?>
             </div>
